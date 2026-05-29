sarim2000/pennywiseai-tracker

Fix GPay UPI SMS duplicates

app/src/main/java/com/pennywiseai/tracker/data/manager/TransactionDeduplication.kt
@@ -1,0 +1,77 @@
+package com.pennywiseai.tracker.data.manager
+
+import com.pennywiseai.tracker.data.database.entity.TransactionEntity
+import java.time.Duration
+
+object TransactionDeduplication {
+    private val upiReferencePattern = Regex("""\d{12}""")
+    val UPI_DUPLICATE_WINDOW: Duration = Duration.ofMinutes(3)
+
+    fun hasUpiReference(transaction: TransactionEntity): Boolean =
+        transaction.reference?.let { upiReferencePattern.matches(it) } == true
+
+    fun isSameUpiTransaction(
+        existing: TransactionEntity,
+        incoming: TransactionEntity,
+        window: Duration = UPI_DUPLICATE_WINDOW
+    ): Boolean {
+        if (!hasUpiReference(existing) || !hasUpiReference(incoming)) return false
+        if (existing.reference != incoming.reference) return false
+        if (existing.transactionType != incoming.transactionType) return false
+        if (existing.currency != incoming.currency) return false
+        if (existing.amount.compareTo(incoming.amount) != 0) return false
+        if (!accountsMatch(existing.accountNumber, incoming.accountNumber)) return false
+
+        val gap = Duration.between(existing.dateTime, incoming.dateTime).abs()
+        return gap <= window
+    }
+
+    fun shouldReplaceWithIncoming(
+        existing: TransactionEntity,
+        incoming: TransactionEntity
+    ): Boolean {
+        if (!isSameUpiTransaction(existing, incoming)) return false
+
+        val existingIsPartnerBank = existing.bankName.equals("State Bank of India", ignoreCase = true)
+        val incomingIsPartnerBank = incoming.bankName.equals("State Bank of India", ignoreCase = true)
+        if (existingIsPartnerBank && !incomingIsPartnerBank) return true
+
+        return existing.balanceAfter == null && incoming.balanceAfter != null
+    }
+
+    fun duplicateIdsToDelete(transactions: List<TransactionEntity>): List<Long> {
+        return transactions
+            .filter { !it.isDeleted && hasUpiReference(it) }
+            .groupBy {
+                DuplicateKey(
+                    reference = it.reference.orEmpty(),
+                    amount = it.amount.stripTrailingZeros().toPlainString(),
+                    accountNumber = it.accountNumber.orEmpty(),
+                    transactionType = it.transactionType.name,
+                    currency = it.currency,
+                    date = it.dateTime.toLocalDate().toString()
+                )
+            }
+            .values
+            .flatMap { group ->
+                group.sortedWith(compareBy<TransactionEntity> { it.dateTime }.thenBy { it.id })
+                    .drop(1)
+                    .map { it.id }
+            }
+    }
+
+    private fun accountsMatch(existingAccount: String?, incomingAccount: String?): Boolean {
+        return existingAccount.isNullOrBlank() ||
+                incomingAccount.isNullOrBlank() ||
+                existingAccount == incomingAccount
+    }
+
+    private data class DuplicateKey(
+        val reference: String,
+        val amount: String,
+        val accountNumber: String,
+        val transactionType: String,
+        val currency: String,
+        val date: String
+    )
+}

app/src/test/java/com/pennywiseai/tracker/data/manager/TransactionDeduplicationTest.kt
@@ -1,0 +1,97 @@
+package com.pennywiseai.tracker.data.manager
+
+import com.pennywiseai.tracker.data.database.entity.TransactionEntity
+import com.pennywiseai.tracker.data.database.entity.TransactionType
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+class TransactionDeduplicationTest {
+
+    private val baseTime = LocalDateTime.of(2025, 12, 26, 19, 2, 1)
+
+    @Test
+    fun `matches same UPI reference from partner and account bank`() {
+        val partnerBank = transaction(
+            id = 1,
+            bankName = "State Bank of India",
+            dateTime = baseTime,
+            balanceAfter = null
+        )
+        val accountBank = transaction(
+            id = 2,
+            bankName = "South Indian Bank",
+            dateTime = baseTime.plusMinutes(2),
+            balanceAfter = BigDecimal("34567.67")
+        )
+
+        assertTrue(TransactionDeduplication.isSameUpiTransaction(partnerBank, accountBank))
+        assertTrue(TransactionDeduplication.shouldReplaceWithIncoming(partnerBank, accountBank))
+    }
+
+    @Test
+    fun `does not match outside duplicate window`() {
+        val first = transaction(id = 1, dateTime = baseTime)
+        val later = transaction(id = 2, dateTime = baseTime.plusMinutes(4))
+
+        assertFalse(TransactionDeduplication.isSameUpiTransaction(first, later))
+    }
+
+    @Test
+    fun `does not match different account with same reference`() {
+        val first = transaction(id = 1, accountNumber = "2468")
+        val otherAccount = transaction(id = 2, accountNumber = "1357")
+
+        assertFalse(TransactionDeduplication.isSameUpiTransaction(first, otherAccount))
+    }
+
+    @Test
+    fun `does not match non UPI references`() {
+        val first = transaction(id = 1, reference = "ABC123")
+        val second = transaction(id = 2, reference = "ABC123")
+
+        assertFalse(TransactionDeduplication.isSameUpiTransaction(first, second))
+    }
+
+    @Test
+    fun `cleanup keeps earliest transaction per reference amount account and day`() {
+        val transactions = listOf(
+            transaction(id = 3, dateTime = baseTime.plusMinutes(2)),
+            transaction(id = 1, dateTime = baseTime),
+            transaction(id = 2, dateTime = baseTime.plusMinutes(1)),
+            transaction(id = 4, dateTime = baseTime.plusDays(1)),
+            transaction(id = 5, amount = BigDecimal("15001.00")),
+            transaction(id = 6, accountNumber = "1357")
+        )
+
+        assertEquals(listOf(2L, 3L), TransactionDeduplication.duplicateIdsToDelete(transactions))
+    }
+
+    private fun transaction(
+        id: Long,
+        amount: BigDecimal = BigDecimal("15000.00"),
+        accountNumber: String = "2468",
+        bankName: String = "South Indian Bank",
+        reference: String = "111222333444",
+        dateTime: LocalDateTime = baseTime,
+        balanceAfter: BigDecimal? = BigDecimal("34567.67")
+    ): TransactionEntity = TransactionEntity(
+        id = id,
+        amount = amount,
+        merchantName = "Sample Merchant",
+        category = "Others",
+        transactionType = TransactionType.INCOME,
+        dateTime = dateTime,
+        smsBody = "sample sms",
+        bankName = bankName,
+        smsSender = "SIBSMS",
+        accountNumber = accountNumber,
+        balanceAfter = balanceAfter,
+        transactionHash = "hash-$id",
+        currency = "INR",
+        reference = reference
+    )
+}

ritesh-kanwar/Cashiro

app/src/main/java/com/ritesh/cashiro/data/manager/TransactionDeduplication.kt
@@ -1,0 +1,77 @@
+package com.ritesh.cashiro.data.manager
+
+import com.ritesh.cashiro.data.database.entity.TransactionEntity
+import java.time.Duration
+
+object TransactionDeduplication {
+    private val upiReferencePattern = Regex("""\d{12}""")
+    val UPI_DUPLICATE_WINDOW: Duration = Duration.ofMinutes(3)
+
+    fun hasUpiReference(transaction: TransactionEntity): Boolean =
+        transaction.reference?.let { upiReferencePattern.matches(it) } == true
+
+    fun isSameUpiTransaction(
+        existing: TransactionEntity,
+        incoming: TransactionEntity,
+        window: Duration = UPI_DUPLICATE_WINDOW
+    ): Boolean {
+        if (!hasUpiReference(existing) || !hasUpiReference(incoming)) return false
+        if (existing.reference != incoming.reference) return false
+        if (existing.transactionType != incoming.transactionType) return false
+        if (existing.currency != incoming.currency) return false
+        if (existing.amount.compareTo(incoming.amount) != 0) return false
+        if (!accountsMatch(existing.accountNumber, incoming.accountNumber)) return false
+
+        val gap = Duration.between(existing.dateTime, incoming.dateTime).abs()
+        return gap <= window
+    }
+
+    fun shouldReplaceWithIncoming(
+        existing: TransactionEntity,
+        incoming: TransactionEntity
+    ): Boolean {
+        if (!isSameUpiTransaction(existing, incoming)) return false
+
+        val existingIsPartnerBank = existing.bankName.equals("State Bank of India", ignoreCase = true)
+        val incomingIsPartnerBank = incoming.bankName.equals("State Bank of India", ignoreCase = true)
+        if (existingIsPartnerBank && !incomingIsPartnerBank) return true
+
+        return existing.balanceAfter == null && incoming.balanceAfter != null
+    }
+
+    fun duplicateIdsToDelete(transactions: List<TransactionEntity>): List<Long> {
+        return transactions
+            .filter { !it.isDeleted && hasUpiReference(it) }
+            .groupBy {
+                DuplicateKey(
+                    reference = it.reference.orEmpty(),
+                    amount = it.amount.stripTrailingZeros().toPlainString(),
+                    accountNumber = it.accountNumber.orEmpty(),
+                    transactionType = it.transactionType.name,
+                    currency = it.currency,
+                    date = it.dateTime.toLocalDate().toString()
+                )
+            }
+            .values
+            .flatMap { group ->
+                group.sortedWith(compareBy<TransactionEntity> { it.dateTime }.thenBy { it.id })
+                    .drop(1)
+                    .map { it.id }
+            }
+    }
+
+    private fun accountsMatch(existingAccount: String?, incomingAccount: String?): Boolean {
+        return existingAccount.isNullOrBlank() ||
+                incomingAccount.isNullOrBlank() ||
+                existingAccount == incomingAccount
+    }
+
+    private data class DuplicateKey(
+        val reference: String,
+        val amount: String,
+        val accountNumber: String,
+        val transactionType: String,
+        val currency: String,
+        val date: String
+    )
+}

app/src/test/java/com/ritesh/cashiro/data/manager/TransactionDeduplicationTest.kt
@@ -1,0 +1,97 @@
+package com.ritesh.cashiro.data.manager
+
+import com.ritesh.cashiro.data.database.entity.TransactionEntity
+import com.ritesh.cashiro.data.database.entity.TransactionType
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+class TransactionDeduplicationTest {
+
+    private val baseTime = LocalDateTime.of(2025, 12, 26, 19, 2, 1)
+
+    @Test
+    fun `matches same UPI reference from partner and account bank`() {
+        val partnerBank = transaction(
+            id = 1,
+            bankName = "State Bank of India",
+            dateTime = baseTime,
+            balanceAfter = null
+        )
+        val accountBank = transaction(
+            id = 2,
+            bankName = "South Indian Bank",
+            dateTime = baseTime.plusMinutes(2),
+            balanceAfter = BigDecimal("34567.67")
+        )
+
+        assertTrue(TransactionDeduplication.isSameUpiTransaction(partnerBank, accountBank))
+        assertTrue(TransactionDeduplication.shouldReplaceWithIncoming(partnerBank, accountBank))
+    }
+
+    @Test
+    fun `does not match outside duplicate window`() {
+        val first = transaction(id = 1, dateTime = baseTime)
+        val later = transaction(id = 2, dateTime = baseTime.plusMinutes(4))
+
+        assertFalse(TransactionDeduplication.isSameUpiTransaction(first, later))
+    }
+
+    @Test
+    fun `does not match different account with same reference`() {
+        val first = transaction(id = 1, accountNumber = "2468")
+        val otherAccount = transaction(id = 2, accountNumber = "1357")
+
+        assertFalse(TransactionDeduplication.isSameUpiTransaction(first, otherAccount))
+    }
+
+    @Test
+    fun `does not match non UPI references`() {
+        val first = transaction(id = 1, reference = "ABC123")
+        val second = transaction(id = 2, reference = "ABC123")
+
+        assertFalse(TransactionDeduplication.isSameUpiTransaction(first, second))
+    }
+
+    @Test
+    fun `cleanup keeps earliest transaction per reference amount account and day`() {
+        val transactions = listOf(
+            transaction(id = 3, dateTime = baseTime.plusMinutes(2)),
+            transaction(id = 1, dateTime = baseTime),
+            transaction(id = 2, dateTime = baseTime.plusMinutes(1)),
+            transaction(id = 4, dateTime = baseTime.plusDays(1)),
+            transaction(id = 5, amount = BigDecimal("15001.00")),
+            transaction(id = 6, accountNumber = "1357")
+        )
+
+        assertEquals(listOf(2L, 3L), TransactionDeduplication.duplicateIdsToDelete(transactions))
+    }
+
+    private fun transaction(
+        id: Long,
+        amount: BigDecimal = BigDecimal("15000.00"),
+        accountNumber: String = "2468",
+        bankName: String = "South Indian Bank",
+        reference: String = "111222333444",
+        dateTime: LocalDateTime = baseTime,
+        balanceAfter: BigDecimal? = BigDecimal("34567.67")
+    ): TransactionEntity = TransactionEntity(
+        id = id,
+        amount = amount,
+        merchantName = "Sample Merchant",
+        category = "Others",
+        transactionType = TransactionType.INCOME,
+        dateTime = dateTime,
+        smsBody = "sample sms",
+        bankName = bankName,
+        smsSender = "SIBSMS",
+        accountNumber = accountNumber,
+        balanceAfter = balanceAfter,
+        transactionHash = "hash-$id",
+        currency = "INR",
+        reference = reference
+    )
+}