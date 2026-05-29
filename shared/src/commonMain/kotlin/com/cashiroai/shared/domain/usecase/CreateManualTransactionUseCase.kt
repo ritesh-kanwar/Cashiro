sarim2000/pennywiseai-tracker

fix(ios): observe CurrencyManager in all screens so currency changes update live
- Add hardcoded currency symbol dictionary (44 currencies) to fix
  locale-dependent symbol resolution returning wrong symbols
- Add @ObservedObject for CurrencyManager in HomeScreen, analytics,
  budgets, and accounts screens so currency changes propagate
- Make "See All" on Home screen a tappable Button that switches to
  the Transactions tab
- Auto-create account entity when manually adding a transaction with
  bank name and account details (create + update paths)
- Override NumberFormatter.currencySymbol with known symbols to avoid
  locale quirks

shared/src/commonMain/kotlin/com/pennywiseai/shared/domain/usecase/CreateManualTransactionUseCase.kt
@@ -1,17 +1,24 @@
 package com.pennywiseai.shared.domain.usecase
 
+import com.pennywiseai.shared.data.local.entity.SharedAccountBalanceEntity
 import com.pennywiseai.shared.data.model.SharedTransaction
+import com.pennywiseai.shared.data.repository.SharedAccountRepository
 import com.pennywiseai.shared.data.repository.SharedTransactionRepository
 import com.pennywiseai.shared.data.util.currentTimeMillis
 
 class CreateManualTransactionUseCase(
-    private val transactionRepository: SharedTransactionRepository
+    private val transactionRepository: SharedTransactionRepository,
+    private val accountRepository: SharedAccountRepository? = null
 ) {
     suspend fun execute(input: ManualTransactionInput): TransactionMutationResult {
         validate(input)?.let { return TransactionMutationResult.ValidationError(it) }
 
         val now = currentTimeMillis()
         val occurredAt = input.occurredAtEpochMillis ?: now
+        val bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() }
+        val accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() }
+        val currency = input.currency.trim().uppercase()
+
         val id = transactionRepository.insert(
             SharedTransaction(
                 amountMinor = input.amountMinor,
@@ -20,14 +27,41 @@ class CreateManualTransactionUseCase(
                 transactionType = input.transactionType,
                 occurredAtEpochMillis = occurredAt,
                 note = input.note?.trim()?.takeIf { it.isNotEmpty() },
-                currency = input.currency.trim().uppercase(),
-                bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() },
-                accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() },
+                currency = currency,
+                bankName = bankName,
+                accountLast4 = accountLast4,
                 createdAtEpochMillis = now,
                 updatedAtEpochMillis = now
             )
         )
+
+        if (bankName != null) {
+            ensureAccountExists(bankName, accountLast4 ?: "", currency, now)
+        }
+
         return TransactionMutationResult.Success(id)
+    }
+
+    private suspend fun ensureAccountExists(
+        bankName: String,
+        accountLast4: String,
+        currency: String,
+        now: Long
+    ) {
+        val repo = accountRepository ?: return
+        val existing = repo.getLatestBalance(bankName, accountLast4)
+        if (existing == null) {
+            repo.insertBalance(
+                SharedAccountBalanceEntity(
+                    bankName = bankName,
+                    accountLast4 = accountLast4,
+                    timestampEpochMillis = now,
+                    balanceMinor = 0L,
+                    currency = currency,
+                    createdAtEpochMillis = now
+                )
+            )
+        }
     }
 
     private fun validate(input: ManualTransactionInput): String? {

shared/src/commonMain/kotlin/com/pennywiseai/shared/domain/usecase/UpdateManualTransactionUseCase.kt
@@ -1,16 +1,23 @@
 package com.pennywiseai.shared.domain.usecase
 
+import com.pennywiseai.shared.data.local.entity.SharedAccountBalanceEntity
+import com.pennywiseai.shared.data.repository.SharedAccountRepository
 import com.pennywiseai.shared.data.repository.SharedTransactionRepository
 import com.pennywiseai.shared.data.util.currentTimeMillis
 
 class UpdateManualTransactionUseCase(
-    private val transactionRepository: SharedTransactionRepository
+    private val transactionRepository: SharedTransactionRepository,
+    private val accountRepository: SharedAccountRepository? = null
 ) {
     suspend fun execute(transactionId: Long, input: ManualTransactionInput): TransactionMutationResult {
         validate(input)?.let { return TransactionMutationResult.ValidationError(it) }
 
         val existing = transactionRepository.getById(transactionId)
             ?: return TransactionMutationResult.NotFound("Transaction not found")
+
+        val bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() }
+        val accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() }
+        val currency = input.currency.trim().uppercase()
 
         transactionRepository.update(
             existing.copy(
@@ -20,13 +27,40 @@ class UpdateManualTransactionUseCase(
                 transactionType = input.transactionType,
                 occurredAtEpochMillis = input.occurredAtEpochMillis ?: existing.occurredAtEpochMillis,
                 note = input.note?.trim()?.takeIf { it.isNotEmpty() },
-                currency = input.currency.trim().uppercase(),
-                bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() },
-                accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() },
+                currency = currency,
+                bankName = bankName,
+                accountLast4 = accountLast4,
                 updatedAtEpochMillis = currentTimeMillis()
             )
         )
+
+        if (bankName != null) {
+            ensureAccountExists(bankName, accountLast4 ?: "", currency, currentTimeMillis())
+        }
+
         return TransactionMutationResult.Success(transactionId)
+    }
+
+    private suspend fun ensureAccountExists(
+        bankName: String,
+        accountLast4: String,
+        currency: String,
+        now: Long
+    ) {
+        val repo = accountRepository ?: return
+        val existing = repo.getLatestBalance(bankName, accountLast4)
+        if (existing == null) {
+            repo.insertBalance(
+                SharedAccountBalanceEntity(
+                    bankName = bankName,
+                    accountLast4 = accountLast4,
+                    timestampEpochMillis = now,
+                    balanceMinor = 0L,
+                    currency = currency,
+                    createdAtEpochMillis = now
+                )
+            )
+        }
     }
 
     private fun validate(input: ManualTransactionInput): String? {

ritesh-kanwar/Cashiro

shared/src/commonMain/kotlin/com/cashiroai/shared/domain/usecase/CreateManualTransactionUseCase.kt
@@ -1,17 +1,24 @@
 package com.cashiroai.shared.domain.usecase
 
+import com.cashiroai.shared.data.local.entity.SharedAccountBalanceEntity
 import com.cashiroai.shared.data.model.SharedTransaction
+import com.cashiroai.shared.data.repository.SharedAccountRepository
 import com.cashiroai.shared.data.repository.SharedTransactionRepository
 import com.cashiroai.shared.data.util.currentTimeMillis
 
 class CreateManualTransactionUseCase(
-    private val transactionRepository: SharedTransactionRepository
+    private val transactionRepository: SharedTransactionRepository,
+    private val accountRepository: SharedAccountRepository? = null
 ) {
     suspend fun execute(input: ManualTransactionInput): TransactionMutationResult {
         validate(input)?.let { return TransactionMutationResult.ValidationError(it) }
 
         val now = currentTimeMillis()
         val occurredAt = input.occurredAtEpochMillis ?: now
+        val bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() }
+        val accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() }
+        val currency = input.currency.trim().uppercase()
+
         val id = transactionRepository.insert(
             SharedTransaction(
                 amountMinor = input.amountMinor,
@@ -20,14 +27,41 @@ class CreateManualTransactionUseCase(
                 transactionType = input.transactionType,
                 occurredAtEpochMillis = occurredAt,
                 note = input.note?.trim()?.takeIf { it.isNotEmpty() },
-                currency = input.currency.trim().uppercase(),
-                bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() },
-                accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() },
+                currency = currency,
+                bankName = bankName,
+                accountLast4 = accountLast4,
                 createdAtEpochMillis = now,
                 updatedAtEpochMillis = now
             )
         )
+
+        if (bankName != null) {
+            ensureAccountExists(bankName, accountLast4 ?: "", currency, now)
+        }
+
         return TransactionMutationResult.Success(id)
+    }
+
+    private suspend fun ensureAccountExists(
+        bankName: String,
+        accountLast4: String,
+        currency: String,
+        now: Long
+    ) {
+        val repo = accountRepository ?: return
+        val existing = repo.getLatestBalance(bankName, accountLast4)
+        if (existing == null) {
+            repo.insertBalance(
+                SharedAccountBalanceEntity(
+                    bankName = bankName,
+                    accountLast4 = accountLast4,
+                    timestampEpochMillis = now,
+                    balanceMinor = 0L,
+                    currency = currency,
+                    createdAtEpochMillis = now
+                )
+            )
+        }
     }
 
     private fun validate(input: ManualTransactionInput): String? {

shared/src/commonMain/kotlin/com/cashiroai/shared/domain/usecase/UpdateManualTransactionUseCase.kt
@@ -1,16 +1,23 @@
 package com.cashiroai.shared.domain.usecase
 
+import com.cashiroai.shared.data.local.entity.SharedAccountBalanceEntity
+import com.cashiroai.shared.data.repository.SharedAccountRepository
 import com.cashiroai.shared.data.repository.SharedTransactionRepository
 import com.cashiroai.shared.data.util.currentTimeMillis
 
 class UpdateManualTransactionUseCase(
-    private val transactionRepository: SharedTransactionRepository
+    private val transactionRepository: SharedTransactionRepository,
+    private val accountRepository: SharedAccountRepository? = null
 ) {
     suspend fun execute(transactionId: Long, input: ManualTransactionInput): TransactionMutationResult {
         validate(input)?.let { return TransactionMutationResult.ValidationError(it) }
 
         val existing = transactionRepository.getById(transactionId)
             ?: return TransactionMutationResult.NotFound("Transaction not found")
+
+        val bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() }
+        val accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() }
+        val currency = input.currency.trim().uppercase()
 
         transactionRepository.update(
             existing.copy(
@@ -20,13 +27,40 @@ class UpdateManualTransactionUseCase(
                 transactionType = input.transactionType,
                 occurredAtEpochMillis = input.occurredAtEpochMillis ?: existing.occurredAtEpochMillis,
                 note = input.note?.trim()?.takeIf { it.isNotEmpty() },
-                currency = input.currency.trim().uppercase(),
-                bankName = input.bankName?.trim()?.takeIf { it.isNotEmpty() },
-                accountLast4 = input.accountLast4?.trim()?.takeIf { it.isNotEmpty() },
+                currency = currency,
+                bankName = bankName,
+                accountLast4 = accountLast4,
                 updatedAtEpochMillis = currentTimeMillis()
             )
         )
+
+        if (bankName != null) {
+            ensureAccountExists(bankName, accountLast4 ?: "", currency, currentTimeMillis())
+        }
+
         return TransactionMutationResult.Success(transactionId)
+    }
+
+    private suspend fun ensureAccountExists(
+        bankName: String,
+        accountLast4: String,
+        currency: String,
+        now: Long
+    ) {
+        val repo = accountRepository ?: return
+        val existing = repo.getLatestBalance(bankName, accountLast4)
+        if (existing == null) {
+            repo.insertBalance(
+                SharedAccountBalanceEntity(
+                    bankName = bankName,
+                    accountLast4 = accountLast4,
+                    timestampEpochMillis = now,
+                    balanceMinor = 0L,
+                    currency = currency,
+                    createdAtEpochMillis = now
+                )
+            )
+        }
     }
 
     private fun validate(input: ManualTransactionInput): String? {