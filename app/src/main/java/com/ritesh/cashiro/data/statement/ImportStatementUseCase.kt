sarim2000/pennywiseai-tracker

feat: enrich transactions from GPay statements

app/src/main/java/com/pennywiseai/tracker/data/statement/ImportStatementUseCase.kt
@@ -2,15 +2,13 @@ package com.pennywiseai.tracker.data.statement
 
 import android.content.Context
 import android.net.Uri
-import com.pennywiseai.tracker.data.mapper.toEntity
+import com.pennywiseai.tracker.data.database.entity.TransactionEntity
 import com.pennywiseai.tracker.data.repository.TransactionRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.time.Instant
+import java.math.BigDecimal
 import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.ZoneId
 import javax.inject.Inject
 
 class ImportStatementUseCase @Inject constructor(
@@ -33,64 +31,36 @@ class ImportStatementUseCase @Inject constructor(
                 )
             }
 
-            var skippedByHash = 0
-            var skippedByReference = 0
-            var skippedByAmountDate = 0
-
-            val toInsert = parsedTransactions.mapNotNull { parsed ->
-                val hash = parsed.transactionHash?.takeIf { it.isNotBlank() }
-                    ?: parsed.generateTransactionId()
-
-                // Tier 0: Exact re-import detection via hash
-                if (transactionRepository.getTransactionByHash(hash) != null) {
-                    skippedByHash++
-                    return@mapNotNull null
-                }
-
-                // Tier 1: Cross-source dedup via UPI reference
-                val ref = parsed.reference
-                if (ref != null && transactionRepository.getTransactionByReference(ref) != null) {
-                    skippedByReference++
-                    return@mapNotNull null
-                }
-
-                // Tier 2: Cross-source dedup via amount + same calendar day
-                val dateTime = LocalDateTime.ofInstant(
-                    Instant.ofEpochMilli(parsed.timestamp),
-                    ZoneId.systemDefault()
-                )
-                val startOfDay = dateTime.toLocalDate().atStartOfDay()
-                val endOfDay = dateTime.toLocalDate().atTime(LocalTime.MAX)
-
-                val amountMatches = transactionRepository.getTransactionByAmountAndDate(
-                    parsed.amount, startOfDay, endOfDay
-                )
-                if (amountMatches.isNotEmpty()) {
-                    skippedByAmountDate++
-                    return@mapNotNull null
-                }
-
-                parsed.toEntity()
-            }
-
-            if (toInsert.isNotEmpty()) {
-                transactionRepository.insertTransactions(toInsert)
-            }
-
-            val skippedDuplicates = skippedByHash + skippedByReference + skippedByAmountDate
-
-            StatementImportResult.Success(
-                imported = toInsert.size,
-                skippedDuplicates = skippedDuplicates,
-                skippedByReference = skippedByReference,
-                skippedByAmountDate = skippedByAmountDate,
-                skippedByHash = skippedByHash,
-                totalParsed = parsedTransactions.size
-            )
+            StatementImportProcessor(repositoryStore()).process(parsedTransactions)
         } catch (e: Exception) {
             StatementImportResult.Error(
                 e.message ?: "Failed to import statement."
             )
+        }
+    }
+
+    private fun repositoryStore() = object : StatementImportProcessor.TransactionStore {
+        override suspend fun getTransactionByHash(transactionHash: String): TransactionEntity? =
+            transactionRepository.getTransactionByHash(transactionHash)
+
+        override suspend fun findStatementMergeCandidate(
+            transaction: TransactionEntity
+        ): TransactionEntity? =
+            transactionRepository.findStatementMergeCandidate(transaction)
+
+        override suspend fun updateTransaction(transaction: TransactionEntity) {
+            transactionRepository.updateTransaction(transaction)
+        }
+
+        override suspend fun getTransactionByAmountAndDate(
+            amount: BigDecimal,
+            dateStart: LocalDateTime,
+            dateEnd: LocalDateTime
+        ): List<TransactionEntity> =
+            transactionRepository.getTransactionByAmountAndDate(amount, dateStart, dateEnd)
+
+        override suspend fun insertTransactions(transactions: List<TransactionEntity>) {
+            transactionRepository.insertTransactions(transactions)
         }
     }
 }

app/src/main/java/com/pennywiseai/tracker/data/statement/StatementImportProcessor.kt
@@ -1,0 +1,107 @@
+package com.pennywiseai.tracker.data.statement
+
+import com.pennywiseai.parser.core.ParsedTransaction
+import com.pennywiseai.tracker.data.database.entity.TransactionEntity
+import com.pennywiseai.tracker.data.mapper.toEntity
+import java.math.BigDecimal
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
+
+internal class StatementImportProcessor(
+    private val transactionStore: TransactionStore
+) {
+    interface TransactionStore {
+        suspend fun getTransactionByHash(transactionHash: String): TransactionEntity?
+        suspend fun findStatementMergeCandidate(transaction: TransactionEntity): TransactionEntity?
+        suspend fun updateTransaction(transaction: TransactionEntity)
+        suspend fun getTransactionByAmountAndDate(
+            amount: BigDecimal,
+            dateStart: LocalDateTime,
+            dateEnd: LocalDateTime
+        ): List<TransactionEntity>
+
+        suspend fun insertTransactions(transactions: List<TransactionEntity>)
+    }
+
+    suspend fun process(parsedTransactions: List<ParsedTransaction>): StatementImportResult.Success {
+        var skippedByHash = 0
+        var skippedByReference = 0
+        var skippedByAmountDate = 0
+        var enriched = 0
+
+        val toInsert = parsedTransactions.mapNotNull { parsed ->
+            val hash = parsed.transactionHash?.takeIf { it.isNotBlank() }
+                ?: parsed.generateTransactionId()
+            val statementEntity = parsed.toEntity()
+
+            if (transactionStore.getTransactionByHash(hash) != null) {
+                skippedByHash++
+                return@mapNotNull null
+            }
+
+            val ref = parsed.reference
+            if (!ref.isNullOrBlank()) {
+                val existing = transactionStore.findStatementMergeCandidate(statementEntity)
+                if (existing != null) {
+                    val merged = StatementTransactionEnricher.enrich(existing, statementEntity)
+                    if (StatementTransactionEnricher.hasEnrichment(existing, merged)) {
+                        transactionStore.updateTransaction(merged)
+                        enriched++
+                    } else {
+                        skippedByReference++
+                    }
+                    return@mapNotNull null
+                }
+            }
+
+            // Reference-bearing statement rows can still match older SMS imports
+            // that were saved before parsers extracted UPI references.
+            val dateTime = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(parsed.timestamp),
+                ZoneId.systemDefault()
+            )
+            val startOfDay = dateTime.toLocalDate().atStartOfDay()
+            val endOfDay = dateTime.toLocalDate().atTime(LocalTime.MAX)
+
+            val amountMatches = transactionStore.getTransactionByAmountAndDate(
+                parsed.amount, startOfDay, endOfDay
+            )
+            val fallbackMergeCandidates = amountMatches.filter { candidate ->
+                StatementTransactionEnricher.isFallbackStatementMatch(candidate, statementEntity)
+            }
+            if (fallbackMergeCandidates.size == 1) {
+                val existing = fallbackMergeCandidates.single()
+                transactionStore.updateTransaction(
+                    StatementTransactionEnricher.enrich(existing, statementEntity)
+                )
+                enriched++
+                return@mapNotNull null
+            }
+
+            if (amountMatches.isNotEmpty()) {
+                skippedByAmountDate++
+                return@mapNotNull null
+            }
+
+            statementEntity
+        }
+
+        if (toInsert.isNotEmpty()) {
+            transactionStore.insertTransactions(toInsert)
+        }
+
+        val skippedDuplicates = skippedByHash + skippedByReference + skippedByAmountDate
+
+        return StatementImportResult.Success(
+            imported = toInsert.size,
+            skippedDuplicates = skippedDuplicates,
+            skippedByReference = skippedByReference,
+            skippedByAmountDate = skippedByAmountDate,
+            skippedByHash = skippedByHash,
+            enriched = enriched,
+            totalParsed = parsedTransactions.size
+        )
+    }
+}

app/src/main/java/com/pennywiseai/tracker/data/statement/StatementTransactionEnricher.kt
@@ -1,0 +1,115 @@
+package com.pennywiseai.tracker.data.statement
+
+import com.pennywiseai.tracker.data.database.entity.TransactionEntity
+import java.time.Duration
+import java.time.LocalDateTime
+
+object StatementTransactionEnricher {
+    val MATCH_WINDOW: Duration = Duration.ofMinutes(5)
+
+    private val genericValues = setOf(
+        "",
+        "unknown",
+        "unknown merchant",
+        "upi",
+        "upi transaction",
+        "upi payment",
+        "upi credit",
+        "payment",
+        "google pay"
+    )
+
+    fun isStatementMatch(existing: TransactionEntity, statement: TransactionEntity): Boolean {
+        val existingReference = existing.reference?.trim().orEmpty()
+        val statementReference = statement.reference?.trim().orEmpty()
+        if (existingReference.isBlank() || existingReference != statementReference) return false
+        return hasSameTransactionDetails(existing, statement)
+    }
+
+    fun isFallbackStatementMatch(existing: TransactionEntity, statement: TransactionEntity): Boolean {
+        if (!hasSameTransactionDetails(existing, statement)) return false
+        return canUseStatementValue(existing.merchantName, statement.merchantName) ||
+                canUseStatementValue(existing.description, statement.description) ||
+                existing.fromAccount.isNullOrBlank() && !statement.fromAccount.isNullOrBlank() ||
+                existing.toAccount.isNullOrBlank() && !statement.toAccount.isNullOrBlank()
+    }
+
+    fun enrich(existing: TransactionEntity, statement: TransactionEntity): TransactionEntity {
+        var changed = false
+
+        val merchantName = if (canUseStatementValue(existing.merchantName, statement.merchantName)) {
+            changed = true
+            statement.merchantName
+        } else {
+            existing.merchantName
+        }
+
+        val description = if (canUseStatementValue(existing.description, statement.description)) {
+            changed = true
+            statement.description
+        } else {
+            existing.description
+        }
+
+        val fromAccount = if (existing.fromAccount.isNullOrBlank() && !statement.fromAccount.isNullOrBlank()) {
+            changed = true
+            statement.fromAccount
+        } else {
+            existing.fromAccount
+        }
+
+        val toAccount = if (existing.toAccount.isNullOrBlank() && !statement.toAccount.isNullOrBlank()) {
+            changed = true
+            statement.toAccount
+        } else {
+            existing.toAccount
+        }
+
+        return if (changed) {
+            existing.copy(
+                merchantName = merchantName,
+                description = description,
+                fromAccount = fromAccount,
+                toAccount = toAccount,
+                updatedAt = LocalDateTime.now()
+            )
+        } else {
+            existing
+        }
+    }
+
+    fun hasEnrichment(existing: TransactionEntity, enriched: TransactionEntity): Boolean =
+        existing.merchantName != enriched.merchantName ||
+                existing.description != enriched.description ||
+                existing.fromAccount != enriched.fromAccount ||
+                existing.toAccount != enriched.toAccount
+
+    private fun canUseStatementValue(existing: String?, statement: String?): Boolean {
+        val incoming = statement?.trim().orEmpty()
+        if (incoming.isBlank() || isGeneric(incoming)) return false
+
+        val current = existing?.trim().orEmpty()
+        return current.isBlank() || isGeneric(current)
+    }
+
+    private fun hasSameTransactionDetails(
+        existing: TransactionEntity,
+        statement: TransactionEntity
+    ): Boolean {
+        if (existing.amount.compareTo(statement.amount) != 0) return false
+        if (existing.currency != statement.currency) return false
+        if (existing.transactionType != statement.transactionType) return false
+        if (!accountsMatch(existing.accountNumber, statement.accountNumber)) return false
+
+        val gap = Duration.between(existing.dateTime, statement.dateTime).abs()
+        return gap <= MATCH_WINDOW
+    }
+
+    private fun isGeneric(value: String): Boolean =
+        value.trim().lowercase() in genericValues
+
+    private fun accountsMatch(existingAccount: String?, statementAccount: String?): Boolean =
+        existingAccount.isNullOrBlank() ||
+                statementAccount.isNullOrBlank() ||
+                existingAccount == statementAccount
+}

ritesh-kanwar/Cashiro

app/src/main/java/com/ritesh/cashiro/data/statement/ImportStatementUseCase.kt
@@ -2,15 +2,13 @@ package com.ritesh.cashiro.data.statement
 
 import android.content.Context
 import android.net.Uri
-import com.ritesh.cashiro.data.mapper.toEntity
+import com.ritesh.cashiro.data.database.entity.TransactionEntity
 import com.ritesh.cashiro.data.repository.TransactionRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.time.Instant
+import java.math.BigDecimal
 import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.ZoneId
 import javax.inject.Inject
 
 class ImportStatementUseCase @Inject constructor(
@@ -33,64 +31,36 @@ class ImportStatementUseCase @Inject constructor(
                 )
             }
 
-            var skippedByHash = 0
-            var skippedByReference = 0
-            var skippedByAmountDate = 0
-
-            val toInsert = parsedTransactions.mapNotNull { parsed ->
-                val hash = parsed.transactionHash?.takeIf { it.isNotBlank() }
-                    ?: parsed.generateTransactionId()
-
-                // Tier 0: Exact re-import detection via hash
-                if (transactionRepository.getTransactionByHash(hash) != null) {
-                    skippedByHash++
-                    return@mapNotNull null
-                }
-
-                // Tier 1: Cross-source dedup via UPI reference
-                val ref = parsed.reference
-                if (ref != null && transactionRepository.getTransactionByReference(ref) != null) {
-                    skippedByReference++
-                    return@mapNotNull null
-                }
-
-                // Tier 2: Cross-source dedup via amount + same calendar day
-                val dateTime = LocalDateTime.ofInstant(
-                    Instant.ofEpochMilli(parsed.timestamp),
-                    ZoneId.systemDefault()
-                )
-                val startOfDay = dateTime.toLocalDate().atStartOfDay()
-                val endOfDay = dateTime.toLocalDate().atTime(LocalTime.MAX)
-
-                val amountMatches = transactionRepository.getTransactionByAmountAndDate(
-                    parsed.amount, startOfDay, endOfDay
-                )
-                if (amountMatches.isNotEmpty()) {
-                    skippedByAmountDate++
-                    return@mapNotNull null
-                }
-
-                parsed.toEntity()
-            }
-
-            if (toInsert.isNotEmpty()) {
-                transactionRepository.insertTransactions(toInsert)
-            }
-
-            val skippedDuplicates = skippedByHash + skippedByReference + skippedByAmountDate
-
-            StatementImportResult.Success(
-                imported = toInsert.size,
-                skippedDuplicates = skippedDuplicates,
-                skippedByReference = skippedByReference,
-                skippedByAmountDate = skippedByAmountDate,
-                skippedByHash = skippedByHash,
-                totalParsed = parsedTransactions.size
-            )
+            StatementImportProcessor(repositoryStore()).process(parsedTransactions)
         } catch (e: Exception) {
             StatementImportResult.Error(
                 e.message ?: "Failed to import statement."
             )
+        }
+    }
+
+    private fun repositoryStore() = object : StatementImportProcessor.TransactionStore {
+        override suspend fun getTransactionByHash(transactionHash: String): TransactionEntity? =
+            transactionRepository.getTransactionByHash(transactionHash)
+
+        override suspend fun findStatementMergeCandidate(
+            transaction: TransactionEntity
+        ): TransactionEntity? =
+            transactionRepository.findStatementMergeCandidate(transaction)
+
+        override suspend fun updateTransaction(transaction: TransactionEntity) {
+            transactionRepository.updateTransaction(transaction)
+        }
+
+        override suspend fun getTransactionByAmountAndDate(
+            amount: BigDecimal,
+            dateStart: LocalDateTime,
+            dateEnd: LocalDateTime
+        ): List<TransactionEntity> =
+            transactionRepository.getTransactionByAmountAndDate(amount, dateStart, dateEnd)
+
+        override suspend fun insertTransactions(transactions: List<TransactionEntity>) {
+            transactionRepository.insertTransactions(transactions)
         }
     }
 }

app/src/main/java/com/ritesh/cashiro/data/statement/StatementImportProcessor.kt
@@ -1,0 +1,107 @@
+package com.ritesh.cashiro.data.statement
+
+import com.ritesh.parser.core.ParsedTransaction
+import com.ritesh.cashiro.data.database.entity.TransactionEntity
+import com.ritesh.cashiro.data.mapper.toEntity
+import java.math.BigDecimal
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
+
+internal class StatementImportProcessor(
+    private val transactionStore: TransactionStore
+) {
+    interface TransactionStore {
+        suspend fun getTransactionByHash(transactionHash: String): TransactionEntity?
+        suspend fun findStatementMergeCandidate(transaction: TransactionEntity): TransactionEntity?
+        suspend fun updateTransaction(transaction: TransactionEntity)
+        suspend fun getTransactionByAmountAndDate(
+            amount: BigDecimal,
+            dateStart: LocalDateTime,
+            dateEnd: LocalDateTime
+        ): List<TransactionEntity>
+
+        suspend fun insertTransactions(transactions: List<TransactionEntity>)
+    }
+
+    suspend fun process(parsedTransactions: List<ParsedTransaction>): StatementImportResult.Success {
+        var skippedByHash = 0
+        var skippedByReference = 0
+        var skippedByAmountDate = 0
+        var enriched = 0
+
+        val toInsert = parsedTransactions.mapNotNull { parsed ->
+            val hash = parsed.transactionHash?.takeIf { it.isNotBlank() }
+                ?: parsed.generateTransactionId()
+            val statementEntity = parsed.toEntity()
+
+            if (transactionStore.getTransactionByHash(hash) != null) {
+                skippedByHash++
+                return@mapNotNull null
+            }
+
+            val ref = parsed.reference
+            if (!ref.isNullOrBlank()) {
+                val existing = transactionStore.findStatementMergeCandidate(statementEntity)
+                if (existing != null) {
+                    val merged = StatementTransactionEnricher.enrich(existing, statementEntity)
+                    if (StatementTransactionEnricher.hasEnrichment(existing, merged)) {
+                        transactionStore.updateTransaction(merged)
+                        enriched++
+                    } else {
+                        skippedByReference++
+                    }
+                    return@mapNotNull null
+                }
+            }
+
+            // Reference-bearing statement rows can still match older SMS imports
+            // that were saved before parsers extracted UPI references.
+            val dateTime = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(parsed.timestamp),
+                ZoneId.systemDefault()
+            )
+            val startOfDay = dateTime.toLocalDate().atStartOfDay()
+            val endOfDay = dateTime.toLocalDate().atTime(LocalTime.MAX)
+
+            val amountMatches = transactionStore.getTransactionByAmountAndDate(
+                parsed.amount, startOfDay, endOfDay
+            )
+            val fallbackMergeCandidates = amountMatches.filter { candidate ->
+                StatementTransactionEnricher.isFallbackStatementMatch(candidate, statementEntity)
+            }
+            if (fallbackMergeCandidates.size == 1) {
+                val existing = fallbackMergeCandidates.single()
+                transactionStore.updateTransaction(
+                    StatementTransactionEnricher.enrich(existing, statementEntity)
+                )
+                enriched++
+                return@mapNotNull null
+            }
+
+            if (amountMatches.isNotEmpty()) {
+                skippedByAmountDate++
+                return@mapNotNull null
+            }
+
+            statementEntity
+        }
+
+        if (toInsert.isNotEmpty()) {
+            transactionStore.insertTransactions(toInsert)
+        }
+
+        val skippedDuplicates = skippedByHash + skippedByReference + skippedByAmountDate
+
+        return StatementImportResult.Success(
+            imported = toInsert.size,
+            skippedDuplicates = skippedDuplicates,
+            skippedByReference = skippedByReference,
+            skippedByAmountDate = skippedByAmountDate,
+            skippedByHash = skippedByHash,
+            enriched = enriched,
+            totalParsed = parsedTransactions.size
+        )
+    }
+}

app/src/main/java/com/ritesh/cashiro/data/statement/StatementTransactionEnricher.kt
@@ -1,0 +1,115 @@
+package com.ritesh.cashiro.data.statement
+
+import com.ritesh.cashiro.data.database.entity.TransactionEntity
+import java.time.Duration
+import java.time.LocalDateTime
+
+object StatementTransactionEnricher {
+    val MATCH_WINDOW: Duration = Duration.ofMinutes(5)
+
+    private val genericValues = setOf(
+        "",
+        "unknown",
+        "unknown merchant",
+        "upi",
+        "upi transaction",
+        "upi payment",
+        "upi credit",
+        "payment",
+        "google pay"
+    )
+
+    fun isStatementMatch(existing: TransactionEntity, statement: TransactionEntity): Boolean {
+        val existingReference = existing.reference?.trim().orEmpty()
+        val statementReference = statement.reference?.trim().orEmpty()
+        if (existingReference.isBlank() || existingReference != statementReference) return false
+        return hasSameTransactionDetails(existing, statement)
+    }
+
+    fun isFallbackStatementMatch(existing: TransactionEntity, statement: TransactionEntity): Boolean {
+        if (!hasSameTransactionDetails(existing, statement)) return false
+        return canUseStatementValue(existing.merchantName, statement.merchantName) ||
+                canUseStatementValue(existing.description, statement.description) ||
+                existing.fromAccount.isNullOrBlank() && !statement.fromAccount.isNullOrBlank() ||
+                existing.toAccount.isNullOrBlank() && !statement.toAccount.isNullOrBlank()
+    }
+
+    fun enrich(existing: TransactionEntity, statement: TransactionEntity): TransactionEntity {
+        var changed = false
+
+        val merchantName = if (canUseStatementValue(existing.merchantName, statement.merchantName)) {
+            changed = true
+            statement.merchantName
+        } else {
+            existing.merchantName
+        }
+
+        val description = if (canUseStatementValue(existing.description, statement.description)) {
+            changed = true
+            statement.description
+        } else {
+            existing.description
+        }
+
+        val fromAccount = if (existing.fromAccount.isNullOrBlank() && !statement.fromAccount.isNullOrBlank()) {
+            changed = true
+            statement.fromAccount
+        } else {
+            existing.fromAccount
+        }
+
+        val toAccount = if (existing.toAccount.isNullOrBlank() && !statement.toAccount.isNullOrBlank()) {
+            changed = true
+            statement.toAccount
+        } else {
+            existing.toAccount
+        }
+
+        return if (changed) {
+            existing.copy(
+                merchantName = merchantName,
+                description = description,
+                fromAccount = fromAccount,
+                toAccount = toAccount,
+                updatedAt = LocalDateTime.now()
+            )
+        } else {
+            existing
+        }
+    }
+
+    fun hasEnrichment(existing: TransactionEntity, enriched: TransactionEntity): Boolean =
+        existing.merchantName != enriched.merchantName ||
+                existing.description != enriched.description ||
+                existing.fromAccount != enriched.fromAccount ||
+                existing.toAccount != enriched.toAccount
+
+    private fun canUseStatementValue(existing: String?, statement: String?): Boolean {
+        val incoming = statement?.trim().orEmpty()
+        if (incoming.isBlank() || isGeneric(incoming)) return false
+
+        val current = existing?.trim().orEmpty()
+        return current.isBlank() || isGeneric(current)
+    }
+
+    private fun hasSameTransactionDetails(
+        existing: TransactionEntity,
+        statement: TransactionEntity
+    ): Boolean {
+        if (existing.amount.compareTo(statement.amount) != 0) return false
+        if (existing.currency != statement.currency) return false
+        if (existing.transactionType != statement.transactionType) return false
+        if (!accountsMatch(existing.accountNumber, statement.accountNumber)) return false
+
+        val gap = Duration.between(existing.dateTime, statement.dateTime).abs()
+        return gap <= MATCH_WINDOW
+    }
+
+    private fun isGeneric(value: String): Boolean =
+        value.trim().lowercase() in genericValues
+
+    private fun accountsMatch(existingAccount: String?, statementAccount: String?): Boolean =
+        existingAccount.isNullOrBlank() ||
+                statementAccount.isNullOrBlank() ||
+                existingAccount == statementAccount
+}