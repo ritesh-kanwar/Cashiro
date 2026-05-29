sarim2000/pennywiseai-tracker

feat(ios): allow renaming accounts after import
Add renameAccount() across DAO, repository, facade, and iOS layers.
Enables editing bank name in account detail screen so users can rename
"UPI (PhonePe)" to the actual bank (e.g. "HDFC Bank"). Rename
propagates to both account balances and linked transactions.

shared/src/commonMain/kotlin/com/pennywiseai/shared/data/repository/RoomSharedAccountRepository.kt
@@ -2,13 +2,15 @@ package com.pennywiseai.shared.data.repository
 
 import com.pennywiseai.shared.data.local.dao.SharedAccountBalanceDao
 import com.pennywiseai.shared.data.local.dao.SharedCardDao
+import com.pennywiseai.shared.data.local.dao.SharedTransactionDao
 import com.pennywiseai.shared.data.local.entity.SharedAccountBalanceEntity
 import com.pennywiseai.shared.data.local.entity.SharedCardEntity
 import kotlinx.coroutines.flow.Flow
 
 class RoomSharedAccountRepository(
     private val balanceDao: SharedAccountBalanceDao,
-    private val cardDao: SharedCardDao
+    private val cardDao: SharedCardDao,
+    private val transactionDao: SharedTransactionDao? = null
 ) : SharedAccountRepository {
     override fun observeBalances(): Flow<List<SharedAccountBalanceEntity>> = balanceDao.observeAll()
     override fun observeCards(): Flow<List<SharedCardEntity>> = cardDao.observeAll()
@@ -22,4 +24,8 @@ class RoomSharedAccountRepository(
     override suspend fun deleteByAccount(bankName: String, accountLast4: String) =
         balanceDao.deleteByAccount(bankName, accountLast4)
     override suspend fun deleteCardById(id: Long) = cardDao.deleteById(id)
+    override suspend fun renameAccount(oldBankName: String, newBankName: String, accountLast4: String) {
+        balanceDao.renameAccount(oldBankName, newBankName, accountLast4)
+        transactionDao?.renameAccountInTransactions(oldBankName, newBankName, accountLast4)
+    }
 }

ritesh-kanwar/Cashiro

shared/src/commonMain/kotlin/com/cashiroai/shared/data/repository/RoomSharedAccountRepository.kt
@@ -2,13 +2,15 @@ package com.cashiroai.shared.data.repository
 
 import com.cashiroai.shared.data.local.dao.SharedAccountBalanceDao
 import com.cashiroai.shared.data.local.dao.SharedCardDao
+import com.cashiroai.shared.data.local.dao.SharedTransactionDao
 import com.cashiroai.shared.data.local.entity.SharedAccountBalanceEntity
 import com.cashiroai.shared.data.local.entity.SharedCardEntity
 import kotlinx.coroutines.flow.Flow
 
 class RoomSharedAccountRepository(
     private val balanceDao: SharedAccountBalanceDao,
-    private val cardDao: SharedCardDao
+    private val cardDao: SharedCardDao,
+    private val transactionDao: SharedTransactionDao? = null
 ) : SharedAccountRepository {
     override fun observeBalances(): Flow<List<SharedAccountBalanceEntity>> = balanceDao.observeAll()
     override fun observeCards(): Flow<List<SharedCardEntity>> = cardDao.observeAll()
@@ -22,4 +24,8 @@ class RoomSharedAccountRepository(
     override suspend fun deleteByAccount(bankName: String, accountLast4: String) =
         balanceDao.deleteByAccount(bankName, accountLast4)
     override suspend fun deleteCardById(id: Long) = cardDao.deleteById(id)
+    override suspend fun renameAccount(oldBankName: String, newBankName: String, accountLast4: String) {
+        balanceDao.renameAccount(oldBankName, newBankName, accountLast4)
+        transactionDao?.renameAccountInTransactions(oldBankName, newBankName, accountLast4)
+    }
 }