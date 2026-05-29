sarim2000/pennywiseai-tracker

Combined changes fixing various bugs (#220)
* add my changes (#2)

* feat(parser): improved PNB bank SMS parsing

* feat(parser): add IMPS reference and account extraction for PNB and South Indian Bank

- PNBBankParser: add patterns for 'a/c no XX340' account format, 'debited for Rs' amount, and 'IMPS Ref no' reference
- SouthIndianBankParser: fix IMPS reference extraction for 'Info: IMPS/PUNB/123456789012/' format, improve merchant extraction
- Add test cases for IMPS transfer messages

* Better parallelization reader (#3)

* fix: refactor and update optimizedsmsreaderworker

Now 2 to 3X faster reading and more maintainable code

fix: warnings fixed

fix: ETA fixed

* fix: missing import

* feat: upgrade to foojay as suggested by android studio

* upgrade to agp 9.0.1

* feat: set default SMS scan period to all time

* fix: show the reference number not sms sender in reference part of trasnaction screen

* fix: further fixes to pnb bank parser to handle a edge case tx where a de3bit is form one acc and send to another as a expense/transfer to a 3rd party acc via imps

* fix: Rewrite GPayPdfParser to handle GPay PDF extraction format
PDF text extraction splits each transaction across multiple lines (date, year, time each on their own line; date appears *before* the `Paid to` anchor). The original single-regex + `SimpleDateFormat` approach couldn't handle this.

Key changes:
- Replace `SimpleDateFormat` with manual 3-part date assembly (`01 Sep,` + `2025` + `03:02 PM`)
- Add pre-anchor line buffer so date lines aren't discarded before block assignment
- Fix `Received from` blocks losing their amount by rejecting `Paid to Bank NNNN` (ends with 4 digits) as a transaction anchor
- Replace fragile last4/bank extraction with `(.+\D)(\d{4})$` covering both `Paid by` and `Paid to` account lines
- Tag each line by semantic type; use anchor-based windowing instead of string splitting

* update to latest gradle

* update workflows to use best jdk for this gradle ver

* fix: ad

parser-core/src/test/kotlin/PNBBankParserTest.kt
@@ -1,0 +1,186 @@
+package com.pennywiseai.parser.core.bank
+
+import com.pennywiseai.parser.core.TransactionType
+import com.pennywiseai.parser.core.bank.PNBBankParser
+import com.pennywiseai.parser.core.test.ExpectedTransaction
+import com.pennywiseai.parser.core.test.ParserTestCase
+import com.pennywiseai.parser.core.test.ParserTestUtils
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import java.math.BigDecimal
+
+class PNBBankParserTest {
+
+    @TestFactory
+    fun `pnb parser handles transaction alerts`(): List<DynamicTest> {
+        val parser = PNBBankParser()
+
+        ParserTestUtils.printTestHeader(
+            parserName = "PNB Bank",
+            bankName = parser.getBankName(),
+            currency = parser.getCurrency()
+        )
+
+        val testCases = listOf(
+            ParserTestCase(
+                name = "Debit message with XX1234",
+                message = "Ac XX1234 Debited with Rs.5000.00, 20-02-2026 07:47:16. Aval Bal Rs.27000.00 CR. Helpline 18001800/18002021-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("5000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00")
+                )
+            ),
+            ParserTestCase(
+                name = "Debit message with card info",
+                message = "A/c XX1234 debited with Rs.5000.00,21-11-2025 13:23:22 thru card XX9239  . Out of 5 free txn on PNB ATM, you utilized 1 txn. Chrgs applicable as per policy. Bal 27000.00 CR. If not done, fwd SMS to 9264192641 to block card/call 18001800/18002021-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("5000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00"),
+                    merchant = "Card XX9239"
+                )
+            ),
+            ParserTestCase(
+                name = "Debit message with VA sender",
+                message = "Ac XX1234 Debited with Rs.5000.00, 16-02-2026 10:04:09. Aval Bal Rs.27000.00 CR. Helpline 18001800/18002021-PNB",
+                sender = "VA-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("5000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00")
+                )
+            ),
+            ParserTestCase(
+                name = "Debit message with long account number",
+                message = "Ac XXXXXXXX00341234 Debited with Rs.10000.00, 20-06-2025 08:18:35. Aval Bal Rs.27000.00 CR. Helpline 18001800/18002021-PNB",
+                sender = "VK-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("10000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00")
+                )
+            ),
+            ParserTestCase(
+                name = "Auto-Pay activation message",
+                message = "Dear Customer, auto pay facility has been successfully activated on your Punjab National Bank Card XX4356 for Rs. 75000.00, from Google Clouds. An initial amount of Rs. 2.00 has been debited from your account. Google Clouds can initiate subsequent transactions for a max amount upto Rs. 75000.00. You will receive notification with the transaction amount prior to any subsequent debits initiated by Google Clouds. Manage / cancel your Auto-Pay facility with ID RTy243262532g via https://www.sihub.in/man",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("2.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "4356",
+                    merchant = "Google Clouds"
+                )
+            ),
+            ParserTestCase(
+                name = "IMPS transfer debit message",
+                message = "Your a/c no XX1234 is debited for Rs 1000 on 01-01-25 12:00:00 and a/c XX456 credited (IMPS Ref no 123456789012) .If not done by you, pl. forward this SMS from registered mobile to 9264092640 to report unauthorized txn & block IBS/MBS. Download PNB ONE.-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("1000"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    reference = "123456789012",
+                    merchant = "IMPS Transfer"
+                )
+            ),
+            ParserTestCase(
+                name = "IMPS transfer debit to different account",
+                message = "Your a/c no XX847 is debited for Rs 3200 on 11-07-25 03:14:52 and a/c XX291 credited (IMPS Ref no 712845931206) .If not done by you, pl. forward this SMS from registered mobile to 9264092640 to report unauthorized txn & block IBS/MBS. Download PNB ONE.-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("3200"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "847",
+                    reference = "712845931206",
+                    merchant = "IMPS Transfer"
+                )
+            ),
+            ParserTestCase(
+                name = "IMPS transfer original message format",
+                message = "Your a/c no XX847 is debited for Rs 6140 on 15-07-25 09:37:44 and a/c XX583 credited (IMPS Ref no 713092476185) .If not done by you, pl. forward this SMS from registered mobile to 9264092640 to report unauthorized txn & block IBS/MBS. Download PNB ONE.-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("6140"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "847",
+                    reference = "713092476185",
+                    merchant = "IMPS Transfer"
+                )
+            ),
+            ParserTestCase(
+                name = "UPI credit message with Ref ID",
+                message = "Ac XX7582 Credited with Rs.4750.00 11-07-2025 08:53:21 thru UPI . Aval Bal Rs.82431.67 CR. (UPI Ref ID:714628305917) Helpline 18001800/18002021-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("4750.00"),
+                    currency = "INR",
+                    type = TransactionType.INCOME,
+                    accountLast4 = "7582",
+                    reference = "714628305917",
+                    balance = BigDecimal("82431.67"),
+                    merchant = "UPI Transaction"
+                )
+            )
+        )
+
+        val handleChecks = listOf(
+            "VM-PNBSMS-S" to true,
+            "VA-PNBSMS-S" to true,
+            "VK-PNBSMS-S" to true,
+            "AX-PNBSMS-S" to true,
+            "PNBBNK" to true,
+            "UNKNOWN" to false
+        )
+
+
+        return ParserTestUtils.runTestSuite(
+            parser = parser,
+            testCases = testCases,
+            handleCases = handleChecks,
+            suiteName = "PNB Parser"
+        )
+    }
+
+    @TestFactory
+    fun `pnb mandate creation is treated as subscription not expense`(): List<DynamicTest> {
+        val parser = PNBBankParser()
+        val sender = "AX-PNBSMS-S"
+        val message =
+            "Your UPI-Mandate is successfully created towards Google for Rs.1500.00 from A/c No.XXXXXX4356. UMN:1d478c77808c410281f435rer5qwerty6@ybl-PNB"
+
+        return listOf(
+            dynamicTest("detect mandate notification") {
+                assertEquals(true, parser.isUPIMandateNotification(message))
+            },
+            dynamicTest("do not parse mandate creation as transaction") {
+                assertNull(parser.parse(message, sender, System.currentTimeMillis()))
+            },
+            dynamicTest("parse mandate subscription details") {
+                val mandate = parser.parseUPIMandateSubscription(message)
+                assertNotNull(mandate)
+                assertEquals(BigDecimal("1500.00"), mandate?.amount)
+                assertEquals("Google", mandate?.merchant)
+                assertEquals("1d478c77808c410281f435rer5qwerty6@ybl-PNB", mandate?.umn)
+            }
+        )
+    }
+}

ritesh-kanwar/Cashiro

parser-core/src/test/kotlin/PNBBankParserTest.kt
@@ -1,0 +1,186 @@
+package com.ritesh.parser.core.bank
+
+import com.ritesh.parser.core.TransactionType
+import com.ritesh.parser.core.bank.PNBBankParser
+import com.ritesh.parser.core.test.ExpectedTransaction
+import com.ritesh.parser.core.test.ParserTestCase
+import com.ritesh.parser.core.test.ParserTestUtils
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import java.math.BigDecimal
+
+class PNBBankParserTest {
+
+    @TestFactory
+    fun `pnb parser handles transaction alerts`(): List<DynamicTest> {
+        val parser = PNBBankParser()
+
+        ParserTestUtils.printTestHeader(
+            parserName = "PNB Bank",
+            bankName = parser.getBankName(),
+            currency = parser.getCurrency()
+        )
+
+        val testCases = listOf(
+            ParserTestCase(
+                name = "Debit message with XX1234",
+                message = "Ac XX1234 Debited with Rs.5000.00, 20-02-2026 07:47:16. Aval Bal Rs.27000.00 CR. Helpline 18001800/18002021-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("5000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00")
+                )
+            ),
+            ParserTestCase(
+                name = "Debit message with card info",
+                message = "A/c XX1234 debited with Rs.5000.00,21-11-2025 13:23:22 thru card XX9239  . Out of 5 free txn on PNB ATM, you utilized 1 txn. Chrgs applicable as per policy. Bal 27000.00 CR. If not done, fwd SMS to 9264192641 to block card/call 18001800/18002021-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("5000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00"),
+                    merchant = "Card XX9239"
+                )
+            ),
+            ParserTestCase(
+                name = "Debit message with VA sender",
+                message = "Ac XX1234 Debited with Rs.5000.00, 16-02-2026 10:04:09. Aval Bal Rs.27000.00 CR. Helpline 18001800/18002021-PNB",
+                sender = "VA-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("5000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00")
+                )
+            ),
+            ParserTestCase(
+                name = "Debit message with long account number",
+                message = "Ac XXXXXXXX00341234 Debited with Rs.10000.00, 20-06-2025 08:18:35. Aval Bal Rs.27000.00 CR. Helpline 18001800/18002021-PNB",
+                sender = "VK-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("10000.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    balance = BigDecimal("27000.00")
+                )
+            ),
+            ParserTestCase(
+                name = "Auto-Pay activation message",
+                message = "Dear Customer, auto pay facility has been successfully activated on your Punjab National Bank Card XX4356 for Rs. 75000.00, from Google Clouds. An initial amount of Rs. 2.00 has been debited from your account. Google Clouds can initiate subsequent transactions for a max amount upto Rs. 75000.00. You will receive notification with the transaction amount prior to any subsequent debits initiated by Google Clouds. Manage / cancel your Auto-Pay facility with ID RTy243262532g via https://www.sihub.in/man",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("2.00"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "4356",
+                    merchant = "Google Clouds"
+                )
+            ),
+            ParserTestCase(
+                name = "IMPS transfer debit message",
+                message = "Your a/c no XX1234 is debited for Rs 1000 on 01-01-25 12:00:00 and a/c XX456 credited (IMPS Ref no 123456789012) .If not done by you, pl. forward this SMS from registered mobile to 9264092640 to report unauthorized txn & block IBS/MBS. Download PNB ONE.-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("1000"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "1234",
+                    reference = "123456789012",
+                    merchant = "IMPS Transfer"
+                )
+            ),
+            ParserTestCase(
+                name = "IMPS transfer debit to different account",
+                message = "Your a/c no XX847 is debited for Rs 3200 on 11-07-25 03:14:52 and a/c XX291 credited (IMPS Ref no 712845931206) .If not done by you, pl. forward this SMS from registered mobile to 9264092640 to report unauthorized txn & block IBS/MBS. Download PNB ONE.-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("3200"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "847",
+                    reference = "712845931206",
+                    merchant = "IMPS Transfer"
+                )
+            ),
+            ParserTestCase(
+                name = "IMPS transfer original message format",
+                message = "Your a/c no XX847 is debited for Rs 6140 on 15-07-25 09:37:44 and a/c XX583 credited (IMPS Ref no 713092476185) .If not done by you, pl. forward this SMS from registered mobile to 9264092640 to report unauthorized txn & block IBS/MBS. Download PNB ONE.-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("6140"),
+                    currency = "INR",
+                    type = TransactionType.EXPENSE,
+                    accountLast4 = "847",
+                    reference = "713092476185",
+                    merchant = "IMPS Transfer"
+                )
+            ),
+            ParserTestCase(
+                name = "UPI credit message with Ref ID",
+                message = "Ac XX7582 Credited with Rs.4750.00 11-07-2025 08:53:21 thru UPI . Aval Bal Rs.82431.67 CR. (UPI Ref ID:714628305917) Helpline 18001800/18002021-PNB",
+                sender = "VM-PNBSMS-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("4750.00"),
+                    currency = "INR",
+                    type = TransactionType.INCOME,
+                    accountLast4 = "7582",
+                    reference = "714628305917",
+                    balance = BigDecimal("82431.67"),
+                    merchant = "UPI Transaction"
+                )
+            )
+        )
+
+        val handleChecks = listOf(
+            "VM-PNBSMS-S" to true,
+            "VA-PNBSMS-S" to true,
+            "VK-PNBSMS-S" to true,
+            "AX-PNBSMS-S" to true,
+            "PNBBNK" to true,
+            "UNKNOWN" to false
+        )
+
+
+        return ParserTestUtils.runTestSuite(
+            parser = parser,
+            testCases = testCases,
+            handleCases = handleChecks,
+            suiteName = "PNB Parser"
+        )
+    }
+
+    @TestFactory
+    fun `pnb mandate creation is treated as subscription not expense`(): List<DynamicTest> {
+        val parser = PNBBankParser()
+        val sender = "AX-PNBSMS-S"
+        val message =
+            "Your UPI-Mandate is successfully created towards Google for Rs.1500.00 from A/c No.XXXXXX4356. UMN:1d478c77808c410281f435rer5qwerty6@ybl-PNB"
+
+        return listOf(
+            dynamicTest("detect mandate notification") {
+                assertEquals(true, parser.isUPIMandateNotification(message))
+            },
+            dynamicTest("do not parse mandate creation as transaction") {
+                assertNull(parser.parse(message, sender, System.currentTimeMillis()))
+            },
+            dynamicTest("parse mandate subscription details") {
+                val mandate = parser.parseUPIMandateSubscription(message)
+                assertNotNull(mandate)
+                assertEquals(BigDecimal("1500.00"), mandate?.amount)
+                assertEquals("Google", mandate?.merchant)
+                assertEquals("1d478c77808c410281f435rer5qwerty6@ybl-PNB", mandate?.umn)
+            }
+        )
+    }
+}