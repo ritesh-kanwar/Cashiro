sarim2000/pennywiseai-tracker

feat: ingest bank app notifications
Add NotificationListenerService to capture transaction notifications from
whitelisted banking apps, providing an alternative data source when SMS
is delayed or unavailable.

- BankNotificationListenerService with package whitelist config
- BankNotification Room entity, DAO, and repository
- Notification access toggle in PermissionScreen and SettingsScreen
- Database v35 with auto-migration from v34

app/src/main/java/com/pennywiseai/tracker/data/database/dao/BankNotificationDao.kt
@@ -1,0 +1,22 @@
+package com.pennywiseai.tracker.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.pennywiseai.tracker.data.database.entity.BankNotificationEntity
+
+@Dao
+interface BankNotificationDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(notification: BankNotificationEntity): Long
+
+    @Query(
+        """
+        UPDATE bank_notifications
+        SET processed = :processed, transaction_id = :transactionId
+        WHERE id = :id
+        """
+    )
+    suspend fun updateStatus(id: Long, processed: Boolean, transactionId: Long?)
+}

app/src/main/java/com/pennywiseai/tracker/data/database/entity/BankNotificationEntity.kt
@@ -1,0 +1,43 @@
+package com.pennywiseai.tracker.data.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "bank_notifications",
+    indices = [
+        Index(value = ["package_name", "message_hash"], unique = true)
+    ]
+)
+data class BankNotificationEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long = 0,
+
+    @ColumnInfo(name = "package_name")
+    val packageName: String,
+
+    @ColumnInfo(name = "sender_alias")
+    val senderAlias: String,
+
+    @ColumnInfo(name = "message_body")
+    val messageBody: String,
+
+    @ColumnInfo(name = "message_hash")
+    val messageHash: String,
+
+    @ColumnInfo(name = "posted_at")
+    val postedAt: LocalDateTime,
+
+    @ColumnInfo(name = "processed")
+    val processed: Boolean = false,
+
+    @ColumnInfo(name = "transaction_id")
+    val transactionId: Long? = null,
+
+    @ColumnInfo(name = "created_at")
+    val createdAt: LocalDateTime = LocalDateTime.now()
+)

app/src/main/java/com/pennywiseai/tracker/data/repository/BankNotificationRepository.kt
@@ -1,0 +1,50 @@
+package com.pennywiseai.tracker.data.repository
+
+import com.pennywiseai.tracker.data.database.dao.BankNotificationDao
+import com.pennywiseai.tracker.data.database.entity.BankNotificationEntity
+import java.security.MessageDigest
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class BankNotificationRepository @Inject constructor(
+    private val dao: BankNotificationDao
+) {
+
+    suspend fun logNotification(
+        packageName: String,
+        senderAlias: String,
+        messageBody: String,
+        postedAtMillis: Long
+    ): Long {
+        val postedAt = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(postedAtMillis),
+            ZoneId.systemDefault()
+        )
+
+        val messageHash = hash("$packageName|$senderAlias|$messageBody")
+
+        val entity = BankNotificationEntity(
+            packageName = packageName,
+            senderAlias = senderAlias,
+            messageBody = messageBody,
+            messageHash = messageHash,
+            postedAt = postedAt
+        )
+
+        return dao.insert(entity)
+    }
+
+    suspend fun markProcessed(id: Long, transactionId: Long?) {
+        dao.updateStatus(id, processed = true, transactionId = transactionId)
+    }
+
+    private fun hash(input: String): String {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val bytes = digest.digest(input.toByteArray())
+        return bytes.joinToString("") { "%02x".format(it) }
+    }
+}

app/src/main/java/com/pennywiseai/tracker/receiver/BankNotificationConfig.kt
@@ -1,0 +1,41 @@
+package com.pennywiseai.tracker.receiver
+
+import android.app.Notification
+
+/**
+ * Configuration and helpers for ingesting bank notifications.
+ *
+ * Only notifications from the allowed package list are processed for
+ * transaction parsing to preserve user privacy.
+ */
+object BankNotificationConfig {
+
+    private val allowedPackages: Map<String, String> = mapOf(
+        // Faysal Bank (Pakistan)
+        "com.avanza.ambitwizfbl" to "com.avanza.ambitwizfbl"
+    )
+
+    fun isAllowed(packageName: String): Boolean =
+        allowedPackages.containsKey(packageName.lowercase())
+
+    fun senderAlias(packageName: String): String =
+        allowedPackages[packageName.lowercase()] ?: packageName
+
+    fun extractMessage(notification: Notification): String {
+        val extras = notification.extras ?: return ""
+
+        val textParts = buildList {
+            extras.getCharSequence(Notification.EXTRA_BIG_TEXT)?.let { add(it) }
+            extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES)?.forEach { add(it) }
+            extras.getCharSequence(Notification.EXTRA_TEXT)?.let { add(it) }
+            extras.getCharSequence(Notification.EXTRA_SUMMARY_TEXT)?.let { add(it) }
+        }
+
+        if (textParts.isNotEmpty()) {
+            val merged = textParts.joinToString("\n") { it.toString() }.trim()
+            if (merged.isNotBlank()) return merged
+        }
+
+        return extras.getCharSequence(Notification.EXTRA_TITLE)?.toString()?.trim().orEmpty()
+    }
+}

app/src/main/java/com/pennywiseai/tracker/receiver/BankNotificationListenerService.kt
@@ -1,0 +1,97 @@
+package com.pennywiseai.tracker.receiver
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+import com.pennywiseai.tracker.data.repository.BankNotificationRepository
+import com.pennywiseai.tracker.data.manager.SmsTransactionProcessor
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+/**
+ * Notification listener that ingests bank app notifications and routes them
+ * through the existing parser pipeline.
+ */
+class BankNotificationListenerService : NotificationListenerService() {
+
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface NotificationListenerEntryPoint {
+        fun smsTransactionProcessor(): SmsTransactionProcessor
+        fun bankNotificationRepository(): BankNotificationRepository
+    }
+
+    companion object {
+        private const val TAG = "BankNotificationListener"
+    }
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        val packageName = sbn.packageName ?: return
+
+        if (!BankNotificationConfig.isAllowed(packageName)) {
+            return
+        }
+
+        // Skip group summaries to avoid duplicate processing
+        if ((sbn.notification.flags and android.app.Notification.FLAG_GROUP_SUMMARY) != 0) {
+            return
+        }
+
+        val body = BankNotificationConfig.extractMessage(sbn.notification)
+        if (body.isBlank()) {
+            return
+        }
+
+        val entryPoint = EntryPointAccessors.fromApplication(
+            applicationContext,
+            NotificationListenerEntryPoint::class.java
+        )
+        val processor = entryPoint.smsTransactionProcessor()
+        val notificationRepository = entryPoint.bankNotificationRepository()
+        val senderAlias = BankNotificationConfig.senderAlias(packageName)
+        val timestamp = sbn.postTime
+
+        serviceScope.launch {
+            var notificationId: Long? = null
+            try {
+                notificationId = notificationRepository.logNotification(
+                    packageName = packageName,
+                    senderAlias = senderAlias,
+                    messageBody = body,
+                    postedAtMillis = timestamp
+                ).takeIf { it > 0 }
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to store bank notification", e)
+            }
+
+            try {
+                val result = processor.processAndSaveTransaction(
+                    sender = senderAlias,
+                    body = body,
+                    timestamp = timestamp
+                )
+                if (!result.success) {
+                    Log.d(TAG, "Notification skipped: ${result.reason}")
+                } else if (notificationId != null) {
+                    notificationRepository.markProcessed(notificationId, result.transactionId)
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to process bank notification", e)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+}

ritesh-kanwar/Cashiro

app/src/main/java/com/ritesh/cashiro/data/database/dao/BankNotificationDao.kt
@@ -1,0 +1,22 @@
+package com.ritesh.cashiro.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ritesh.cashiro.data.database.entity.BankNotificationEntity
+
+@Dao
+interface BankNotificationDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(notification: BankNotificationEntity): Long
+
+    @Query(
+        """
+        UPDATE bank_notifications
+        SET processed = :processed, transaction_id = :transactionId
+        WHERE id = :id
+        """
+    )
+    suspend fun updateStatus(id: Long, processed: Boolean, transactionId: Long?)
+}

app/src/main/java/com/ritesh/cashiro/data/database/entity/BankNotificationEntity.kt
@@ -1,0 +1,43 @@
+package com.ritesh.cashiro.data.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "bank_notifications",
+    indices = [
+        Index(value = ["package_name", "message_hash"], unique = true)
+    ]
+)
+data class BankNotificationEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long = 0,
+
+    @ColumnInfo(name = "package_name")
+    val packageName: String,
+
+    @ColumnInfo(name = "sender_alias")
+    val senderAlias: String,
+
+    @ColumnInfo(name = "message_body")
+    val messageBody: String,
+
+    @ColumnInfo(name = "message_hash")
+    val messageHash: String,
+
+    @ColumnInfo(name = "posted_at")
+    val postedAt: LocalDateTime,
+
+    @ColumnInfo(name = "processed")
+    val processed: Boolean = false,
+
+    @ColumnInfo(name = "transaction_id")
+    val transactionId: Long? = null,
+
+    @ColumnInfo(name = "created_at")
+    val createdAt: LocalDateTime = LocalDateTime.now()
+)

app/src/main/java/com/ritesh/cashiro/data/repository/BankNotificationRepository.kt
@@ -1,0 +1,50 @@
+package com.ritesh.cashiro.data.repository
+
+import com.ritesh.cashiro.data.database.dao.BankNotificationDao
+import com.ritesh.cashiro.data.database.entity.BankNotificationEntity
+import java.security.MessageDigest
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class BankNotificationRepository @Inject constructor(
+    private val dao: BankNotificationDao
+) {
+
+    suspend fun logNotification(
+        packageName: String,
+        senderAlias: String,
+        messageBody: String,
+        postedAtMillis: Long
+    ): Long {
+        val postedAt = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(postedAtMillis),
+            ZoneId.systemDefault()
+        )
+
+        val messageHash = hash("$packageName|$senderAlias|$messageBody")
+
+        val entity = BankNotificationEntity(
+            packageName = packageName,
+            senderAlias = senderAlias,
+            messageBody = messageBody,
+            messageHash = messageHash,
+            postedAt = postedAt
+        )
+
+        return dao.insert(entity)
+    }
+
+    suspend fun markProcessed(id: Long, transactionId: Long?) {
+        dao.updateStatus(id, processed = true, transactionId = transactionId)
+    }
+
+    private fun hash(input: String): String {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val bytes = digest.digest(input.toByteArray())
+        return bytes.joinToString("") { "%02x".format(it) }
+    }
+}

app/src/main/java/com/ritesh/cashiro/receiver/BankNotificationConfig.kt
@@ -1,0 +1,41 @@
+package com.ritesh.cashiro.receiver
+
+import android.app.Notification
+
+/**
+ * Configuration and helpers for ingesting bank notifications.
+ *
+ * Only notifications from the allowed package list are processed for
+ * transaction parsing to preserve user privacy.
+ */
+object BankNotificationConfig {
+
+    private val allowedPackages: Map<String, String> = mapOf(
+        // Faysal Bank (Pakistan)
+        "com.avanza.ambitwizfbl" to "com.avanza.ambitwizfbl"
+    )
+
+    fun isAllowed(packageName: String): Boolean =
+        allowedPackages.containsKey(packageName.lowercase())
+
+    fun senderAlias(packageName: String): String =
+        allowedPackages[packageName.lowercase()] ?: packageName
+
+    fun extractMessage(notification: Notification): String {
+        val extras = notification.extras ?: return ""
+
+        val textParts = buildList {
+            extras.getCharSequence(Notification.EXTRA_BIG_TEXT)?.let { add(it) }
+            extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES)?.forEach { add(it) }
+            extras.getCharSequence(Notification.EXTRA_TEXT)?.let { add(it) }
+            extras.getCharSequence(Notification.EXTRA_SUMMARY_TEXT)?.let { add(it) }
+        }
+
+        if (textParts.isNotEmpty()) {
+            val merged = textParts.joinToString("\n") { it.toString() }.trim()
+            if (merged.isNotBlank()) return merged
+        }
+
+        return extras.getCharSequence(Notification.EXTRA_TITLE)?.toString()?.trim().orEmpty()
+    }
+}

app/src/main/java/com/ritesh/cashiro/receiver/BankNotificationListenerService.kt
@@ -1,0 +1,97 @@
+package com.ritesh.cashiro.receiver
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+import com.ritesh.cashiro.data.repository.BankNotificationRepository
+import com.ritesh.cashiro.data.manager.SmsTransactionProcessor
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+/**
+ * Notification listener that ingests bank app notifications and routes them
+ * through the existing parser pipeline.
+ */
+class BankNotificationListenerService : NotificationListenerService() {
+
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface NotificationListenerEntryPoint {
+        fun smsTransactionProcessor(): SmsTransactionProcessor
+        fun bankNotificationRepository(): BankNotificationRepository
+    }
+
+    companion object {
+        private const val TAG = "BankNotificationListener"
+    }
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        val packageName = sbn.packageName ?: return
+
+        if (!BankNotificationConfig.isAllowed(packageName)) {
+            return
+        }
+
+        // Skip group summaries to avoid duplicate processing
+        if ((sbn.notification.flags and android.app.Notification.FLAG_GROUP_SUMMARY) != 0) {
+            return
+        }
+
+        val body = BankNotificationConfig.extractMessage(sbn.notification)
+        if (body.isBlank()) {
+            return
+        }
+
+        val entryPoint = EntryPointAccessors.fromApplication(
+            applicationContext,
+            NotificationListenerEntryPoint::class.java
+        )
+        val processor = entryPoint.smsTransactionProcessor()
+        val notificationRepository = entryPoint.bankNotificationRepository()
+        val senderAlias = BankNotificationConfig.senderAlias(packageName)
+        val timestamp = sbn.postTime
+
+        serviceScope.launch {
+            var notificationId: Long? = null
+            try {
+                notificationId = notificationRepository.logNotification(
+                    packageName = packageName,
+                    senderAlias = senderAlias,
+                    messageBody = body,
+                    postedAtMillis = timestamp
+                ).takeIf { it > 0 }
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to store bank notification", e)
+            }
+
+            try {
+                val result = processor.processAndSaveTransaction(
+                    sender = senderAlias,
+                    body = body,
+                    timestamp = timestamp
+                )
+                if (!result.success) {
+                    Log.d(TAG, "Notification skipped: ${result.reason}")
+                } else if (notificationId != null) {
+                    notificationRepository.markProcessed(notificationId, result.transactionId)
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to process bank notification", e)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+}