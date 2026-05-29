sarim2000/pennywiseai-tracker

feat(ios): add onboarding flow, greeting card, and subscriptions CRUD
- Add 3-step onboarding: Welcome, Profile (name + avatar), PDF Import
- Gate MainTabView behind hasCompletedOnboarding @AppStorage flag
- Add GreetingCard component with time-of-day greeting and avatar
- Add Subscriptions list screen with swipe-to-delete
- Add Add/Edit subscription form with category picker
- Add subscription CRUD facade methods (getAllSubscriptions, create,
  update, delete) to shared KMP module
- Register all 9 new Swift files in project.pbxproj

iosApp/iosApp/App/MainTabView.swift
@@ -21,49 +21,57 @@ struct MainTabView: View {
     @State private var selectedTab: AppTab = .home
     @ObservedObject private var themeManager = ThemeManager.shared
     @ObservedObject private var appLockManager = AppLockManager.shared
+    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
 
     var body: some View {
-        ZStack {
-            TabView(selection: $selectedTab) {
-                NavigationStack {
-                    HomeScreen(onSeeAllTransactions: { selectedTab = .transactions })
-                        .navigationTitle("PennyWise")
-                }
-                .tabItem {
-                    Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
-                }
-                .tag(AppTab.home)
+        if hasCompletedOnboarding {
+            ZStack {
+                TabView(selection: $selectedTab) {
+                    NavigationStack {
+                        HomeScreen(onSeeAllTransactions: { selectedTab = .transactions })
+                            .navigationTitle("PennyWise")
+                    }
+                    .tabItem {
+                        Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
+                    }
+                    .tag(AppTab.home)
 
-                NavigationStack {
-                    TransactionListView(facade: PennyWiseSharedFacade())
-                }
-                .tabItem {
-                    Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
-                }
-                .tag(AppTab.transactions)
+                    NavigationStack {
+                        TransactionListView(facade: PennyWiseSharedFacade())
+                    }
+                    .tabItem {
+                        Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
+                    }
+                    .tag(AppTab.transactions)
 
-                NavigationStack {
-                    AnalyticsScreen()
-                }
-                .tabItem {
-                    Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
-                }
-                .tag(AppTab.analytics)
+                    NavigationStack {
+                        AnalyticsScreen()
+                    }
+                    .tabItem {
+                        Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
+                    }
+                    .tag(AppTab.analytics)
 
-                NavigationStack {
-                    SettingsScreen()
+                    NavigationStack {
+                        SettingsScreen()
+                    }
+                    .tabItem {
+                        Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+                    }
+                    .tag(AppTab.settings)
                 }
-                .tabItem {
-                    Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+                .tint(themeManager.accentColor)
+
+                if appLockManager.isLocked {
+                    LockScreenView()
                 }
-                .tag(AppTab.settings)
             }
-            .tint(themeManager.accentColor)
-
-            if appLockManager.isLocked {
-                LockScreenView()
+            .preferredColorScheme(themeManager.colorScheme)
+        } else {
+            OnboardingScreen {
+                hasCompletedOnboarding = true
             }
+            .preferredColorScheme(themeManager.colorScheme)
         }
-        .preferredColorScheme(themeManager.colorScheme)
     }
 }

iosApp/iosApp/Screens/Onboarding/WelcomeStep.swift
@@ -1,0 +1,63 @@
+import SwiftUI
+
+struct WelcomeStep: View {
+    @ObservedObject private var themeManager = ThemeManager.shared
+
+    var body: some View {
+        VStack(spacing: AppSpacing.xl) {
+            Spacer()
+
+            Image(systemName: "chart.bar.doc.horizontal")
+                .font(.system(size: 80))
+                .foregroundColor(themeManager.accentColor)
+
+            VStack(spacing: AppSpacing.sm) {
+                Text("Welcome to PennyWise")
+                    .font(AppTypography.largeTitle)
+                    .multilineTextAlignment(.center)
+
+                Text("Your minimalist expense tracker")
+                    .font(AppTypography.body)
+                    .foregroundColor(.secondary)
+            }
+
+            VStack(alignment: .leading, spacing: AppSpacing.md) {
+                featureBullet(
+                    icon: "dollarsign.circle.fill",
+                    text: "Track expenses effortlessly"
+                )
+                featureBullet(
+                    icon: "chart.pie.fill",
+                    text: "Beautiful charts & insights"
+                )
+                featureBullet(
+                    icon: "lock.shield.fill",
+                    text: "All data stays on your device"
+                )
+            }
+            .padding(.horizontal, AppSpacing.lg)
+
+            Spacer()
+            Spacer()
+        }
+        .padding(AppSpacing.lg)
+    }
+
+    private func featureBullet(icon: String, text: String) -> some View {
+        HStack(spacing: AppSpacing.md) {
+            Image(systemName: icon)
+                .font(.title2)
+                .foregroundColor(themeManager.accentColor)
+                .frame(width: 32)
+
+            Text(text)
+                .font(AppTypography.body)
+        }
+    }
+}
+
+struct WelcomeStep_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeStep()
+    }
+}

iosApp/iosApp/ViewModels/OnboardingViewModel.swift
@@ -1,0 +1,49 @@
+import Foundation
+import PDFKit
+import Shared
+
+class OnboardingViewModel: ObservableObject {
+    @Published var currentStep = 0
+    @Published var importResult: String?
+    @Published var isImporting = false
+
+    private let facade = PennyWiseSharedFacade()
+
+    func importPDF(url: URL) {
+        isImporting = true
+        importResult = nil
+
+        guard let pdfDocument = PDFDocument(url: url) else {
+            importResult = "Could not open PDF file"
+            isImporting = false
+            return
+        }
+
+        var fullText = ""
+        for i in 0..<pdfDocument.pageCount {
+            if let page = pdfDocument.page(at: i), let pageText = page.string {
+                fullText += pageText + "\n"
+            }
+        }
+
+        guard !fullText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
+            importResult = "No text found in PDF"
+            isImporting = false
+            return
+        }
+
+        let snapshot = facade.importStatementTextAndLoadHome(statementText: fullText)
+        if snapshot.lastImportParsed > 0 {
+            importResult = "\(snapshot.lastImportImported) transactions imported, \(snapshot.lastImportSkipped) skipped"
+        } else if let error = snapshot.lastError {
+            importResult = error
+        } else {
+            importResult = "No transactions found in this statement"
+        }
+        isImporting = false
+    }
+
+    func completeOnboarding() {
+        UserDefaults.standard.set(true, forKey: "hasCompletedOnboarding")
+    }
+}

iosApp/iosApp/ViewModels/SubscriptionsViewModel.swift
@@ -1,0 +1,63 @@
+import Foundation
+import Shared
+
+class SubscriptionsViewModel: ObservableObject {
+    private let facade = PennyWiseSharedFacade()
+
+    @Published var subscriptions: [SharedSubscriptionItem] = []
+    @Published var categories: [String] = []
+
+    func loadSubscriptions() {
+        subscriptions = facade.getAllSubscriptions()
+    }
+
+    func loadCategories() {
+        categories = facade.getAllCategories().map { $0.name }
+    }
+
+    func createSubscription(
+        merchantName: String,
+        amountMinor: Int64,
+        category: String?,
+        currency: String
+    ) -> Bool {
+        let success = facade.createSubscription(
+            merchantName: merchantName,
+            amountMinor: amountMinor,
+            category: category,
+            currency: currency
+        )
+        if success {
+            loadSubscriptions()
+        }
+        return success
+    }
+
+    func updateSubscription(
+        id: Int64,
+        merchantName: String,
+        amountMinor: Int64,
+        category: String?,
+        currency: String
+    ) -> Bool {
+        let success = facade.updateSubscription(
+            id: id,
+            merchantName: merchantName,
+            amountMinor: amountMinor,
+            category: category,
+            currency: currency
+        )
+        if success {
+            loadSubscriptions()
+        }
+        return success
+    }
+
+    func deleteSubscription(id: Int64) -> Bool {
+        let success = facade.deleteSubscription(id: id)
+        if success {
+            loadSubscriptions()
+        }
+        return success
+    }
+}

ritesh-kanwar/Cashiro

iosApp/iosApp/App/MainTabView.swift
@@ -21,49 +21,57 @@ struct MainTabView: View {
     @State private var selectedTab: AppTab = .home
     @ObservedObject private var themeManager = ThemeManager.shared
     @ObservedObject private var appLockManager = AppLockManager.shared
+    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
 
     var body: some View {
-        ZStack {
-            TabView(selection: $selectedTab) {
-                NavigationStack {
-                    HomeScreen(onSeeAllTransactions: { selectedTab = .transactions })
-                        .navigationTitle("Cashiro")
-                }
-                .tabItem {
-                    Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
-                }
-                .tag(AppTab.home)
+        if hasCompletedOnboarding {
+            ZStack {
+                TabView(selection: $selectedTab) {
+                    NavigationStack {
+                        HomeScreen(onSeeAllTransactions: { selectedTab = .transactions })
+                            .navigationTitle("Cashiro")
+                    }
+                    .tabItem {
+                        Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
+                    }
+                    .tag(AppTab.home)
 
-                NavigationStack {
-                    TransactionListView(facade: CashiroSharedFacade())
-                }
-                .tabItem {
-                    Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
-                }
-                .tag(AppTab.transactions)
+                    NavigationStack {
+                        TransactionListView(facade: CashiroSharedFacade())
+                    }
+                    .tabItem {
+                        Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
+                    }
+                    .tag(AppTab.transactions)
 
-                NavigationStack {
-                    AnalyticsScreen()
-                }
-                .tabItem {
-                    Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
-                }
-                .tag(AppTab.analytics)
+                    NavigationStack {
+                        AnalyticsScreen()
+                    }
+                    .tabItem {
+                        Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
+                    }
+                    .tag(AppTab.analytics)
 
-                NavigationStack {
-                    SettingsScreen()
+                    NavigationStack {
+                        SettingsScreen()
+                    }
+                    .tabItem {
+                        Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+                    }
+                    .tag(AppTab.settings)
                 }
-                .tabItem {
-                    Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+                .tint(themeManager.accentColor)
+
+                if appLockManager.isLocked {
+                    LockScreenView()
                 }
-                .tag(AppTab.settings)
             }
-            .tint(themeManager.accentColor)
-
-            if appLockManager.isLocked {
-                LockScreenView()
+            .preferredColorScheme(themeManager.colorScheme)
+        } else {
+            OnboardingScreen {
+                hasCompletedOnboarding = true
             }
+            .preferredColorScheme(themeManager.colorScheme)
         }
-        .preferredColorScheme(themeManager.colorScheme)
     }
 }

iosApp/iosApp/Screens/Onboarding/WelcomeStep.swift
@@ -1,0 +1,63 @@
+import SwiftUI
+
+struct WelcomeStep: View {
+    @ObservedObject private var themeManager = ThemeManager.shared
+
+    var body: some View {
+        VStack(spacing: AppSpacing.xl) {
+            Spacer()
+
+            Image(systemName: "chart.bar.doc.horizontal")
+                .font(.system(size: 80))
+                .foregroundColor(themeManager.accentColor)
+
+            VStack(spacing: AppSpacing.sm) {
+                Text("Welcome to Cashiro")
+                    .font(AppTypography.largeTitle)
+                    .multilineTextAlignment(.center)
+
+                Text("Your minimalist expense tracker")
+                    .font(AppTypography.body)
+                    .foregroundColor(.secondary)
+            }
+
+            VStack(alignment: .leading, spacing: AppSpacing.md) {
+                featureBullet(
+                    icon: "dollarsign.circle.fill",
+                    text: "Track expenses effortlessly"
+                )
+                featureBullet(
+                    icon: "chart.pie.fill",
+                    text: "Beautiful charts & insights"
+                )
+                featureBullet(
+                    icon: "lock.shield.fill",
+                    text: "All data stays on your device"
+                )
+            }
+            .padding(.horizontal, AppSpacing.lg)
+
+            Spacer()
+            Spacer()
+        }
+        .padding(AppSpacing.lg)
+    }
+
+    private func featureBullet(icon: String, text: String) -> some View {
+        HStack(spacing: AppSpacing.md) {
+            Image(systemName: icon)
+                .font(.title2)
+                .foregroundColor(themeManager.accentColor)
+                .frame(width: 32)
+
+            Text(text)
+                .font(AppTypography.body)
+        }
+    }
+}
+
+struct WelcomeStep_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeStep()
+    }
+}

iosApp/iosApp/ViewModels/OnboardingViewModel.swift
@@ -1,0 +1,49 @@
+import Foundation
+import PDFKit
+import Shared
+
+class OnboardingViewModel: ObservableObject {
+    @Published var currentStep = 0
+    @Published var importResult: String?
+    @Published var isImporting = false
+
+    private let facade = CashiroSharedFacade()
+
+    func importPDF(url: URL) {
+        isImporting = true
+        importResult = nil
+
+        guard let pdfDocument = PDFDocument(url: url) else {
+            importResult = "Could not open PDF file"
+            isImporting = false
+            return
+        }
+
+        var fullText = ""
+        for i in 0..<pdfDocument.pageCount {
+            if let page = pdfDocument.page(at: i), let pageText = page.string {
+                fullText += pageText + "\n"
+            }
+        }
+
+        guard !fullText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
+            importResult = "No text found in PDF"
+            isImporting = false
+            return
+        }
+
+        let snapshot = facade.importStatementTextAndLoadHome(statementText: fullText)
+        if snapshot.lastImportParsed > 0 {
+            importResult = "\(snapshot.lastImportImported) transactions imported, \(snapshot.lastImportSkipped) skipped"
+        } else if let error = snapshot.lastError {
+            importResult = error
+        } else {
+            importResult = "No transactions found in this statement"
+        }
+        isImporting = false
+    }
+
+    func completeOnboarding() {
+        UserDefaults.standard.set(true, forKey: "hasCompletedOnboarding")
+    }
+}

iosApp/iosApp/ViewModels/SubscriptionsViewModel.swift
@@ -1,0 +1,63 @@
+import Foundation
+import Shared
+
+class SubscriptionsViewModel: ObservableObject {
+    private let facade = CashiroSharedFacade()
+
+    @Published var subscriptions: [SharedSubscriptionItem] = []
+    @Published var categories: [String] = []
+
+    func loadSubscriptions() {
+        subscriptions = facade.getAllSubscriptions()
+    }
+
+    func loadCategories() {
+        categories = facade.getAllCategories().map { $0.name }
+    }
+
+    func createSubscription(
+        merchantName: String,
+        amountMinor: Int64,
+        category: String?,
+        currency: String
+    ) -> Bool {
+        let success = facade.createSubscription(
+            merchantName: merchantName,
+            amountMinor: amountMinor,
+            category: category,
+            currency: currency
+        )
+        if success {
+            loadSubscriptions()
+        }
+        return success
+    }
+
+    func updateSubscription(
+        id: Int64,
+        merchantName: String,
+        amountMinor: Int64,
+        category: String?,
+        currency: String
+    ) -> Bool {
+        let success = facade.updateSubscription(
+            id: id,
+            merchantName: merchantName,
+            amountMinor: amountMinor,
+            category: category,
+            currency: currency
+        )
+        if success {
+            loadSubscriptions()
+        }
+        return success
+    }
+
+    func deleteSubscription(id: Int64) -> Bool {
+        let success = facade.deleteSubscription(id: id)
+        if success {
+            loadSubscriptions()
+        }
+        return success
+    }
+}