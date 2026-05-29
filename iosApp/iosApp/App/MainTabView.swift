sarim2000/pennywiseai-tracker

feat(ios): add settings screens with appearance, app lock, export/import
- Add ThemeManager with Rose Pine accent color palette (12 colors),
  system/light/dark mode, AMOLED dark toggle
- Add AppearanceScreen with theme mode selector, accent color picker
- Add AppLockManager with Face ID/Touch ID via LocalAuthentication,
  configurable lock timeout (immediately/1min/5min/15min)
- Add LockScreenView overlay with biometric re-authentication
- Add ExportImportManager for JSON backup export via share sheet
  and import via document picker
- Add DocumentPicker (UIDocumentPickerViewController wrapper)
- Add FAQScreen with 3 expandable categories (8 Q&As)
- Add AboutScreen with version info and GitHub links
- Reorganize SettingsScreen into 6 sections: Personalization, Data
  Management, Preferences, Security, Support, About
- Apply ThemeManager and AppLockManager globally in MainTabView
- Add NSFaceIDUsageDescription to Info.plist

iosApp/iosApp/App/MainTabView.swift
@@ -19,41 +19,51 @@ enum AppTab: String, CaseIterable {
 
 struct MainTabView: View {
     @State private var selectedTab: AppTab = .home
+    @ObservedObject private var themeManager = ThemeManager.shared
+    @ObservedObject private var appLockManager = AppLockManager.shared
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            NavigationStack {
-                HomeScreen()
-                    .navigationTitle("PennyWise")
-            }
-            .tabItem {
-                Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
-            }
-            .tag(AppTab.home)
+        ZStack {
+            TabView(selection: $selectedTab) {
+                NavigationStack {
+                    HomeScreen()
+                        .navigationTitle("PennyWise")
+                }
+                .tabItem {
+                    Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
+                }
+                .tag(AppTab.home)
 
-            NavigationStack {
-                TransactionListView(facade: PennyWiseSharedFacade())
-            }
-            .tabItem {
-                Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
-            }
-            .tag(AppTab.transactions)
+                NavigationStack {
+                    TransactionListView(facade: PennyWiseSharedFacade())
+                }
+                .tabItem {
+                    Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
+                }
+                .tag(AppTab.transactions)
 
-            NavigationStack {
-                AnalyticsScreen()
-            }
-            .tabItem {
-                Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
-            }
-            .tag(AppTab.analytics)
+                NavigationStack {
+                    AnalyticsScreen()
+                }
+                .tabItem {
+                    Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
+                }
+                .tag(AppTab.analytics)
 
-            NavigationStack {
-                SettingsScreen()
+                NavigationStack {
+                    SettingsScreen()
+                }
+                .tabItem {
+                    Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+                }
+                .tag(AppTab.settings)
             }
-            .tabItem {
-                Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+            .tint(themeManager.accentColor)
+
+            if appLockManager.isLocked {
+                LockScreenView()
             }
-            .tag(AppTab.settings)
         }
+        .preferredColorScheme(themeManager.colorScheme)
     }
 }

iosApp/iosApp/Components/LockScreenView.swift
@@ -1,0 +1,52 @@
+import SwiftUI
+
+struct LockScreenView: View {
+    @ObservedObject var appLockManager = AppLockManager.shared
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.ultraThinMaterial)
+                .ignoresSafeArea()
+
+            VStack(spacing: AppSpacing.lg) {
+                Spacer()
+
+                Image(systemName: "lock.shield.fill")
+                    .font(.system(size: 56))
+                    .foregroundStyle(.secondary)
+
+                VStack(spacing: AppSpacing.sm) {
+                    Text("PennyWise")
+                        .font(AppTypography.title)
+
+                    Text("Locked")
+                        .font(AppTypography.caption)
+                        .foregroundStyle(.secondary)
+                }
+
+                Spacer()
+
+                Button {
+                    appLockManager.authenticate()
+                } label: {
+                    Label(
+                        "Unlock with \(appLockManager.biometricType)",
+                        systemImage: appLockManager.biometricIcon
+                    )
+                    .font(AppTypography.headline)
+                    .frame(maxWidth: .infinity)
+                    .padding(AppSpacing.md)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: AppCornerRadius.medium))
+                }
+                .buttonStyle(.plain)
+                .padding(.horizontal, AppSpacing.xl)
+                .padding(.bottom, AppSpacing.xl)
+            }
+        }
+        .onAppear {
+            appLockManager.authenticate()
+        }
+    }
+}

iosApp/iosApp/Screens/Settings/AboutScreen.swift
@@ -1,0 +1,81 @@
+import SwiftUI
+
+struct AboutScreen: View {
+    private var appVersion: String {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+    }
+
+    private var buildNumber: String {
+        Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "1"
+    }
+
+    var body: some View {
+        List {
+            Section {
+                VStack(spacing: AppSpacing.md) {
+                    Image(systemName: "dollarsign.circle.fill")
+                        .font(.system(size: 72))
+                        .foregroundStyle(.tint)
+
+                    Text("PennyWise")
+                        .font(AppTypography.title)
+
+                    Text("Version \(appVersion) (\(buildNumber))")
+                        .font(AppTypography.caption)
+                        .foregroundStyle(.secondary)
+
+                    Text("AI-powered expense tracker that helps you understand your spending habits.")
+                        .font(AppTypography.body)
+                        .foregroundStyle(.secondary)
+                        .multilineTextAlignment(.center)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, AppSpacing.lg)
+            }
+
+            Section("Links") {
+                Button {
+                    if let url = URL(string: "https://github.com/nicekid1/pennywiseai-tracker") {
+                        UIApplication.shared.open(url)
+                    }
+                } label: {
+                    Label {
+                        Text("GitHub Repository")
+                            .font(AppTypography.body)
+                            .foregroundStyle(.primary)
+                    } icon: {
+                        Image(systemName: "chevron.left.forwardslash.chevron.right")
+                            .foregroundStyle(.blue)
+                    }
+                }
+
+                Button {
+                    if let url = URL(string: "https://github.com/nicekid1/pennywiseai-tracker/issues/new/choose") {
+                        UIApplication.shared.open(url)
+                    }
+                } label: {
+                    Label {
+                        Text("Report an Issue")
+                            .font(AppTypography.body)
+                            .foregroundStyle(.primary)
+                    } icon: {
+                        Image(systemName: "exclamationmark.bubble")
+                            .foregroundStyle(.orange)
+                    }
+                }
+            }
+
+            Section {
+                VStack(spacing: AppSpacing.xs) {
+                    Text("Made with care for mindful spending")
+                        .font(AppTypography.caption)
+                        .foregroundStyle(.secondary)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, AppSpacing.sm)
+            }
+        }
+        .navigationTitle("About")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}

iosApp/iosApp/Screens/Settings/AppLockManager.swift
@@ -1,0 +1,114 @@
+import Foundation
+import LocalAuthentication
+import SwiftUI
+import UIKit
+
+final class AppLockManager: ObservableObject {
+    static let shared = AppLockManager()
+
+    @AppStorage("appLockEnabled") var appLockEnabled: Bool = false
+    @AppStorage("lockTimeoutMinutes") var lockTimeoutMinutes: Int = 0
+
+    @Published var isLocked: Bool = false
+
+    private var lastBackgroundTime: Date?
+
+    var canUseBiometric: Bool {
+        let context = LAContext()
+        var error: NSError?
+        return context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
+    }
+
+    var biometricType: String {
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return "Biometrics"
+        }
+        switch context.biometryType {
+        case .faceID:
+            return "Face ID"
+        case .touchID:
+            return "Touch ID"
+        default:
+            return "Biometrics"
+        }
+    }
+
+    var biometricIcon: String {
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return "lock.fill"
+        }
+        switch context.biometryType {
+        case .faceID:
+            return "faceid"
+        case .touchID:
+            return "touchid"
+        default:
+            return "lock.fill"
+        }
+    }
+
+    private init() {
+        setupNotifications()
+    }
+
+    private func setupNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(didEnterBackground),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(willEnterForeground),
+            name: UIApplication.willEnterForegroundNotification,
+            object: nil
+        )
+    }
+
+    @objc private func didEnterBackground() {
+        guard appLockEnabled else { return }
+        lastBackgroundTime = Date()
+    }
+
+    @objc private func willEnterForeground() {
+        guard appLockEnabled else { return }
+        guard let backgroundTime = lastBackgroundTime else {
+            isLocked = true
+            return
+        }
+        let elapsed = Date().timeIntervalSince(backgroundTime)
+        let timeoutSeconds = TimeInterval(lockTimeoutMinutes * 60)
+        if elapsed >= timeoutSeconds {
+            isLocked = true
+        }
+    }
+
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return
+        }
+        context.evaluatePolicy(
+            .deviceOwnerAuthenticationWithBiometrics,
+            localizedReason: "Unlock PennyWise to access your financial data"
+        ) { [weak self] success, _ in
+            DispatchQueue.main.async {
+                if success {
+                    self?.isLocked = false
+                }
+            }
+        }
+    }
+
+    func lockIfEnabled() {
+        if appLockEnabled {
+            isLocked = true
+        }
+    }
+}

ritesh-kanwar/Cashiro

iosApp/iosApp/App/MainTabView.swift
@@ -19,41 +19,51 @@ enum AppTab: String, CaseIterable {
 
 struct MainTabView: View {
     @State private var selectedTab: AppTab = .home
+    @ObservedObject private var themeManager = ThemeManager.shared
+    @ObservedObject private var appLockManager = AppLockManager.shared
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            NavigationStack {
-                HomeScreen()
-                    .navigationTitle("Cashiro")
-            }
-            .tabItem {
-                Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
-            }
-            .tag(AppTab.home)
+        ZStack {
+            TabView(selection: $selectedTab) {
+                NavigationStack {
+                    HomeScreen()
+                        .navigationTitle("Cashiro")
+                }
+                .tabItem {
+                    Label(AppTab.home.rawValue, systemImage: AppTab.home.icon)
+                }
+                .tag(AppTab.home)
 
-            NavigationStack {
-                TransactionListView(facade: CashiroSharedFacade())
-            }
-            .tabItem {
-                Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
-            }
-            .tag(AppTab.transactions)
+                NavigationStack {
+                    TransactionListView(facade: CashiroSharedFacade())
+                }
+                .tabItem {
+                    Label(AppTab.transactions.rawValue, systemImage: AppTab.transactions.icon)
+                }
+                .tag(AppTab.transactions)
 
-            NavigationStack {
-                AnalyticsScreen()
-            }
-            .tabItem {
-                Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
-            }
-            .tag(AppTab.analytics)
+                NavigationStack {
+                    AnalyticsScreen()
+                }
+                .tabItem {
+                    Label(AppTab.analytics.rawValue, systemImage: AppTab.analytics.icon)
+                }
+                .tag(AppTab.analytics)
 
-            NavigationStack {
-                SettingsScreen()
+                NavigationStack {
+                    SettingsScreen()
+                }
+                .tabItem {
+                    Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+                }
+                .tag(AppTab.settings)
             }
-            .tabItem {
-                Label(AppTab.settings.rawValue, systemImage: AppTab.settings.icon)
+            .tint(themeManager.accentColor)
+
+            if appLockManager.isLocked {
+                LockScreenView()
             }
-            .tag(AppTab.settings)
         }
+        .preferredColorScheme(themeManager.colorScheme)
     }
 }

iosApp/iosApp/Components/LockScreenView.swift
@@ -1,0 +1,52 @@
+import SwiftUI
+
+struct LockScreenView: View {
+    @ObservedObject var appLockManager = AppLockManager.shared
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.ultraThinMaterial)
+                .ignoresSafeArea()
+
+            VStack(spacing: AppSpacing.lg) {
+                Spacer()
+
+                Image(systemName: "lock.shield.fill")
+                    .font(.system(size: 56))
+                    .foregroundStyle(.secondary)
+
+                VStack(spacing: AppSpacing.sm) {
+                    Text("Cashiro")
+                        .font(AppTypography.title)
+
+                    Text("Locked")
+                        .font(AppTypography.caption)
+                        .foregroundStyle(.secondary)
+                }
+
+                Spacer()
+
+                Button {
+                    appLockManager.authenticate()
+                } label: {
+                    Label(
+                        "Unlock with \(appLockManager.biometricType)",
+                        systemImage: appLockManager.biometricIcon
+                    )
+                    .font(AppTypography.headline)
+                    .frame(maxWidth: .infinity)
+                    .padding(AppSpacing.md)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: AppCornerRadius.medium))
+                }
+                .buttonStyle(.plain)
+                .padding(.horizontal, AppSpacing.xl)
+                .padding(.bottom, AppSpacing.xl)
+            }
+        }
+        .onAppear {
+            appLockManager.authenticate()
+        }
+    }
+}

iosApp/iosApp/Screens/Settings/AboutScreen.swift
@@ -1,0 +1,81 @@
+import SwiftUI
+
+struct AboutScreen: View {
+    private var appVersion: String {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+    }
+
+    private var buildNumber: String {
+        Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "1"
+    }
+
+    var body: some View {
+        List {
+            Section {
+                VStack(spacing: AppSpacing.md) {
+                    Image(systemName: "dollarsign.circle.fill")
+                        .font(.system(size: 72))
+                        .foregroundStyle(.tint)
+
+                    Text("Cashiro")
+                        .font(AppTypography.title)
+
+                    Text("Version \(appVersion) (\(buildNumber))")
+                        .font(AppTypography.caption)
+                        .foregroundStyle(.secondary)
+
+                    Text("AI-powered expense tracker that helps you understand your spending habits.")
+                        .font(AppTypography.body)
+                        .foregroundStyle(.secondary)
+                        .multilineTextAlignment(.center)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, AppSpacing.lg)
+            }
+
+            Section("Links") {
+                Button {
+                    if let url = URL(string: "https://github.com/nicekid1/cashiroai-tracker") {
+                        UIApplication.shared.open(url)
+                    }
+                } label: {
+                    Label {
+                        Text("GitHub Repository")
+                            .font(AppTypography.body)
+                            .foregroundStyle(.primary)
+                    } icon: {
+                        Image(systemName: "chevron.left.forwardslash.chevron.right")
+                            .foregroundStyle(.blue)
+                    }
+                }
+
+                Button {
+                    if let url = URL(string: "https://github.com/nicekid1/cashiroai-tracker/issues/new/choose") {
+                        UIApplication.shared.open(url)
+                    }
+                } label: {
+                    Label {
+                        Text("Report an Issue")
+                            .font(AppTypography.body)
+                            .foregroundStyle(.primary)
+                    } icon: {
+                        Image(systemName: "exclamationmark.bubble")
+                            .foregroundStyle(.orange)
+                    }
+                }
+            }
+
+            Section {
+                VStack(spacing: AppSpacing.xs) {
+                    Text("Made with care for mindful spending")
+                        .font(AppTypography.caption)
+                        .foregroundStyle(.secondary)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, AppSpacing.sm)
+            }
+        }
+        .navigationTitle("About")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}

iosApp/iosApp/Screens/Settings/AppLockManager.swift
@@ -1,0 +1,114 @@
+import Foundation
+import LocalAuthentication
+import SwiftUI
+import UIKit
+
+final class AppLockManager: ObservableObject {
+    static let shared = AppLockManager()
+
+    @AppStorage("appLockEnabled") var appLockEnabled: Bool = false
+    @AppStorage("lockTimeoutMinutes") var lockTimeoutMinutes: Int = 0
+
+    @Published var isLocked: Bool = false
+
+    private var lastBackgroundTime: Date?
+
+    var canUseBiometric: Bool {
+        let context = LAContext()
+        var error: NSError?
+        return context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
+    }
+
+    var biometricType: String {
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return "Biometrics"
+        }
+        switch context.biometryType {
+        case .faceID:
+            return "Face ID"
+        case .touchID:
+            return "Touch ID"
+        default:
+            return "Biometrics"
+        }
+    }
+
+    var biometricIcon: String {
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return "lock.fill"
+        }
+        switch context.biometryType {
+        case .faceID:
+            return "faceid"
+        case .touchID:
+            return "touchid"
+        default:
+            return "lock.fill"
+        }
+    }
+
+    private init() {
+        setupNotifications()
+    }
+
+    private func setupNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(didEnterBackground),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(willEnterForeground),
+            name: UIApplication.willEnterForegroundNotification,
+            object: nil
+        )
+    }
+
+    @objc private func didEnterBackground() {
+        guard appLockEnabled else { return }
+        lastBackgroundTime = Date()
+    }
+
+    @objc private func willEnterForeground() {
+        guard appLockEnabled else { return }
+        guard let backgroundTime = lastBackgroundTime else {
+            isLocked = true
+            return
+        }
+        let elapsed = Date().timeIntervalSince(backgroundTime)
+        let timeoutSeconds = TimeInterval(lockTimeoutMinutes * 60)
+        if elapsed >= timeoutSeconds {
+            isLocked = true
+        }
+    }
+
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return
+        }
+        context.evaluatePolicy(
+            .deviceOwnerAuthenticationWithBiometrics,
+            localizedReason: "Unlock Cashiro to access your financial data"
+        ) { [weak self] success, _ in
+            DispatchQueue.main.async {
+                if success {
+                    self?.isLocked = false
+                }
+            }
+        }
+    }
+
+    func lockIfEnabled() {
+        if appLockEnabled {
+            isLocked = true
+        }
+    }
+}