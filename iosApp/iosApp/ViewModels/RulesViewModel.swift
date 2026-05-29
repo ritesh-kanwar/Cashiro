sarim2000/pennywiseai-tracker

feat(ios): add rules, carousels, Notion avatars, and fix UUID compilation
- Add Smart Rules CRUD screen with keyword-to-category mapping
- Add rule CRUD facade methods (getAllRules, create, update, toggle, delete)
- Fix ExperimentalUuidApi error: replace kotlin.uuid.Uuid.random() with
  timestamp-based ID generation
- Add BudgetCarousel with progress bars and color-coded spend tracking
- Add AccountCarousel with balance display and bank name
- Add both carousels to HomeScreen between greeting and transactions
- Copy 10 Notion-style avatar PNGs from Android to iOS asset catalog
- Replace SF Symbol avatars with Notion illustrations in ProfileStep
  and GreetingCard
- Make onboarding name field more prominent with placeholder text

iosApp/iosApp/ViewModels/RulesViewModel.swift
@@ -1,0 +1,73 @@
+import Foundation
+import Shared
+
+class RulesViewModel: ObservableObject {
+    private let facade = PennyWiseSharedFacade()
+
+    @Published var rules: [SharedRuleItem] = []
+    @Published var categories: [String] = []
+
+    func loadRules() {
+        rules = facade.getAllRules()
+    }
+
+    func loadCategories() {
+        categories = facade.getAllCategories().map { $0.name }
+    }
+
+    func createRule(
+        name: String,
+        conditionsJson: String,
+        actionsJson: String,
+        priority: Int32
+    ) -> Bool {
+        let success = facade.createRule(
+            name: name,
+            conditionsJson: conditionsJson,
+            actionsJson: actionsJson,
+            priority: priority
+        )
+        if success {
+            loadRules()
+        }
+        return success
+    }
+
+    func updateRule(
+        id: String,
+        name: String,
+        conditionsJson: String,
+        actionsJson: String,
+        isEnabled: Bool,
+        priority: Int32
+    ) -> Bool {
+        let success = facade.updateRule(
+            id: id,
+            name: name,
+            conditionsJson: conditionsJson,
+            actionsJson: actionsJson,
+            isEnabled: isEnabled,
+            priority: priority
+        )
+        if success {
+            loadRules()
+        }
+        return success
+    }
+
+    func toggleRule(id: String, isEnabled: Bool) -> Bool {
+        let success = facade.toggleRule(id: id, isEnabled: isEnabled)
+        if success {
+            loadRules()
+        }
+        return success
+    }
+
+    func deleteRule(id: String) -> Bool {
+        let success = facade.deleteRule(id: id)
+        if success {
+            loadRules()
+        }
+        return success
+    }
+}

ritesh-kanwar/Cashiro

iosApp/iosApp/ViewModels/RulesViewModel.swift
@@ -1,0 +1,73 @@
+import Foundation
+import Shared
+
+class RulesViewModel: ObservableObject {
+    private let facade = CashiroSharedFacade()
+
+    @Published var rules: [SharedRuleItem] = []
+    @Published var categories: [String] = []
+
+    func loadRules() {
+        rules = facade.getAllRules()
+    }
+
+    func loadCategories() {
+        categories = facade.getAllCategories().map { $0.name }
+    }
+
+    func createRule(
+        name: String,
+        conditionsJson: String,
+        actionsJson: String,
+        priority: Int32
+    ) -> Bool {
+        let success = facade.createRule(
+            name: name,
+            conditionsJson: conditionsJson,
+            actionsJson: actionsJson,
+            priority: priority
+        )
+        if success {
+            loadRules()
+        }
+        return success
+    }
+
+    func updateRule(
+        id: String,
+        name: String,
+        conditionsJson: String,
+        actionsJson: String,
+        isEnabled: Bool,
+        priority: Int32
+    ) -> Bool {
+        let success = facade.updateRule(
+            id: id,
+            name: name,
+            conditionsJson: conditionsJson,
+            actionsJson: actionsJson,
+            isEnabled: isEnabled,
+            priority: priority
+        )
+        if success {
+            loadRules()
+        }
+        return success
+    }
+
+    func toggleRule(id: String, isEnabled: Bool) -> Bool {
+        let success = facade.toggleRule(id: id, isEnabled: isEnabled)
+        if success {
+            loadRules()
+        }
+        return success
+    }
+
+    func deleteRule(id: String) -> Bool {
+        let success = facade.deleteRule(id: id)
+        if success {
+            loadRules()
+        }
+        return success
+    }
+}