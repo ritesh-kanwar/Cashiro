sarim2000/pennywiseai-tracker

fix(parser): fix 5 parser bugs from user feedback
- ICICI: NEFT "credited to beneficiary" returns EXPENSE (was TRANSFER)
- BOI: extract merchant from NEFT inward pattern (By NEFTINWARD ref/NAME)
- HDFC: filter out NACH mandate processing notifications as non-transactions
- Jupiter/CSB: return null merchant when no merchant in SMS, type → CREDIT
- Canara: add regression test for RTGS MF redemption → INCOME

parser-core/src/main/kotlin/com/pennywiseai/parser/core/bank/JupiterBankParser.kt
@@ -1,5 +1,6 @@
 package com.pennywiseai.parser.core.bank
 
+import com.pennywiseai.parser.core.TransactionType
 import java.math.BigDecimal
 
 /**
@@ -60,19 +61,18 @@ class JupiterBankParser : BankParser() {
     }
 
     override fun extractMerchant(message: String, sender: String): String? {
-        // For Jupiter/CSB credit card transactions, the merchant info is usually not in the message
-        // These are typically just marked as credit card transactions
+        return super.extractMerchant(message, sender)
+    }
 
+    override fun extractTransactionType(message: String): TransactionType? {
         val lowerMessage = message.lowercase()
 
-        // Check for specific transaction types
-        return when {
-            lowerMessage.contains("edge csb bank rupay credit card") -> "Credit Card Payment"
-            lowerMessage.contains("jupiter csb edge") -> "Credit Card Payment"
-            lowerMessage.contains("credit card") -> "Credit Card Payment"
-            lowerMessage.contains("upi") -> "UPI Transaction"
-            else -> super.extractMerchant(message, sender) ?: "Jupiter Transaction"
+        if (lowerMessage.contains("credit card") &&
+            (lowerMessage.contains("debited") || lowerMessage.contains("spent") || lowerMessage.contains("charged"))) {
+            return TransactionType.CREDIT
         }
+
+        return super.extractTransactionType(message)
     }
 
     override fun extractAccountLast4(message: String): String? {

parser-core/src/test/kotlin/com/pennywiseai/parser/core/bank/CanaraBankParserTest.kt
@@ -1,0 +1,34 @@
+package com.pennywiseai.parser.core.bank
+
+import com.pennywiseai.parser.core.TransactionType
+import com.pennywiseai.parser.core.test.ExpectedTransaction
+import com.pennywiseai.parser.core.test.ParserTestCase
+import com.pennywiseai.parser.core.test.ParserTestUtils
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.math.BigDecimal
+
+class CanaraBankParserTest {
+
+    private val parser = CanaraBankParser()
+
+    @TestFactory
+    fun `canara bank parser handles key paths`(): List<DynamicTest> {
+        val cases = listOf(
+            ParserTestCase(
+                name = "RTGS MF redemption credit typed as INCOME",
+                message = "An amount of INR 13,30,614.75 has been credited to XXXX6785 on 02/12/2025 towards RTGS by Sender AXIS MUTUAL FUND REDEMPTION PO, IFSC UTIB0000004, Sender A/c XXXX9108, AXIS BANK, MUMBAI BRANCH, UTR UTIBR72025120200011461, Total Avail. Bal INR 2679815.88- Canara Bank",
+                sender = "VA-CANBNK-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("1330614.75"),
+                    currency = "INR",
+                    type = TransactionType.INCOME,
+                    merchant = "AXIS MUTUAL FUND REDEMPTION PO",
+                    accountLast4 = "9108"
+                )
+            )
+        )
+
+        return ParserTestUtils.runTestSuite(parser, cases)
+    }
+}

parser-core/src/test/kotlin/com/pennywiseai/parser/core/bank/JupiterBankParserTest.kt
@@ -1,0 +1,40 @@
+package com.pennywiseai.parser.core.bank
+
+import com.pennywiseai.parser.core.TransactionType
+import com.pennywiseai.parser.core.test.ExpectedTransaction
+import com.pennywiseai.parser.core.test.ParserTestCase
+import com.pennywiseai.parser.core.test.ParserTestUtils
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.math.BigDecimal
+
+class JupiterBankParserTest {
+
+    private val parser = JupiterBankParser()
+
+    @TestFactory
+    fun `jupiter parser handles key paths`(): List<DynamicTest> {
+        val testCases = listOf(
+            ParserTestCase(
+                name = "CSB credit card debit via UPI",
+                message = "Rs.25.00 debited to your Edge CSB Bank RuPay Credit Card ending 6788 on 3/18/26, 6:39 PM - (UPI Ref no.702711160776). To dispute, call 8655055086.",
+                sender = "VK-JTEDGE-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("25.00"),
+                    currency = "INR",
+                    type = TransactionType.CREDIT,
+                    merchant = null,
+                    accountLast4 = "6788",
+                    reference = "702711160776"
+                )
+            )
+        )
+
+        return ParserTestUtils.runTestSuite(
+            parser = parser,
+            testCases = testCases,
+            handleCases = listOf("VK-JTEDGE-S" to true, "HDFCBK" to false),
+            suiteName = "Jupiter Parser"
+        )
+    }
+}

ritesh-kanwar/Cashiro

parser-core/src/main/kotlin/com/ritesh/parser/core/bank/JupiterBankParser.kt
@@ -1,5 +1,6 @@
 package com.ritesh.parser.core.bank
 
+import com.ritesh.parser.core.TransactionType
 import java.math.BigDecimal
 
 /**
@@ -60,19 +61,18 @@ class JupiterBankParser : BankParser() {
     }
 
     override fun extractMerchant(message: String, sender: String): String? {
-        // For Jupiter/CSB credit card transactions, the merchant info is usually not in the message
-        // These are typically just marked as credit card transactions
+        return super.extractMerchant(message, sender)
+    }
 
+    override fun extractTransactionType(message: String): TransactionType? {
         val lowerMessage = message.lowercase()
 
-        // Check for specific transaction types
-        return when {
-            lowerMessage.contains("edge csb bank rupay credit card") -> "Credit Card Payment"
-            lowerMessage.contains("jupiter csb edge") -> "Credit Card Payment"
-            lowerMessage.contains("credit card") -> "Credit Card Payment"
-            lowerMessage.contains("upi") -> "UPI Transaction"
-            else -> super.extractMerchant(message, sender) ?: "Jupiter Transaction"
+        if (lowerMessage.contains("credit card") &&
+            (lowerMessage.contains("debited") || lowerMessage.contains("spent") || lowerMessage.contains("charged"))) {
+            return TransactionType.CREDIT
         }
+
+        return super.extractTransactionType(message)
     }
 
     override fun extractAccountLast4(message: String): String? {

parser-core/src/test/kotlin/com/ritesh/parser/core/bank/CanaraBankParserTest.kt
@@ -1,0 +1,34 @@
+package com.ritesh.parser.core.bank
+
+import com.ritesh.parser.core.TransactionType
+import com.ritesh.parser.core.test.ExpectedTransaction
+import com.ritesh.parser.core.test.ParserTestCase
+import com.ritesh.parser.core.test.ParserTestUtils
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.math.BigDecimal
+
+class CanaraBankParserTest {
+
+    private val parser = CanaraBankParser()
+
+    @TestFactory
+    fun `canara bank parser handles key paths`(): List<DynamicTest> {
+        val cases = listOf(
+            ParserTestCase(
+                name = "RTGS MF redemption credit typed as INCOME",
+                message = "An amount of INR 13,30,614.75 has been credited to XXXX6785 on 02/12/2025 towards RTGS by Sender AXIS MUTUAL FUND REDEMPTION PO, IFSC UTIB0000004, Sender A/c XXXX9108, AXIS BANK, MUMBAI BRANCH, UTR UTIBR72025120200011461, Total Avail. Bal INR 2679815.88- Canara Bank",
+                sender = "VA-CANBNK-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("1330614.75"),
+                    currency = "INR",
+                    type = TransactionType.INCOME,
+                    merchant = "AXIS MUTUAL FUND REDEMPTION PO",
+                    accountLast4 = "9108"
+                )
+            )
+        )
+
+        return ParserTestUtils.runTestSuite(parser, cases)
+    }
+}

parser-core/src/test/kotlin/com/ritesh/parser/core/bank/JupiterBankParserTest.kt
@@ -1,0 +1,40 @@
+package com.ritesh.parser.core.bank
+
+import com.ritesh.parser.core.TransactionType
+import com.ritesh.parser.core.test.ExpectedTransaction
+import com.ritesh.parser.core.test.ParserTestCase
+import com.ritesh.parser.core.test.ParserTestUtils
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.math.BigDecimal
+
+class JupiterBankParserTest {
+
+    private val parser = JupiterBankParser()
+
+    @TestFactory
+    fun `jupiter parser handles key paths`(): List<DynamicTest> {
+        val testCases = listOf(
+            ParserTestCase(
+                name = "CSB credit card debit via UPI",
+                message = "Rs.25.00 debited to your Edge CSB Bank RuPay Credit Card ending 6788 on 3/18/26, 6:39 PM - (UPI Ref no.702711160776). To dispute, call 8655055086.",
+                sender = "VK-JTEDGE-S",
+                expected = ExpectedTransaction(
+                    amount = BigDecimal("25.00"),
+                    currency = "INR",
+                    type = TransactionType.CREDIT,
+                    merchant = null,
+                    accountLast4 = "6788",
+                    reference = "702711160776"
+                )
+            )
+        )
+
+        return ParserTestUtils.runTestSuite(
+            parser = parser,
+            testCases = testCases,
+            handleCases = listOf("VK-JTEDGE-S" to true, "HDFCBK" to false),
+            suiteName = "Jupiter Parser"
+        )
+    }
+}