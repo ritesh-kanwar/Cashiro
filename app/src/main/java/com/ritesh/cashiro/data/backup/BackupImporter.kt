sarim2000/pennywiseai-tracker

fix: backup import NPE when older backups omit new list fields (#341)
* fix: NPE on backup import when older backups omit new list fields

Tester hit a hard crash on import: 'Attempt to invoke interface method
java.util.Iterator java.lang.Iterable.iterator() on a null object
reference'. The crash dialog blocked all data restoration.

Root cause: Gson sets every property via reflection during JSON
deserialisation, bypassing the Kotlin '= emptyList()' default
initialisers on DatabaseSnapshot. When an older backup file lacks one
of our newer list keys ('loans', 'transactionGroups', 'profiles',
'budgetMonthSnapshots', 'budgetCategoryMonthSnapshots' — and any
older list that pre-dates the source backup), Gson ends up storing
null in a property whose declared type is non-null List<T>. The first
.forEach / .isNotEmpty() / .map call against that property throws an
NPE even though every call site looks safe.

Fix: normalise the snapshot once, right after gson.fromJson, by
running every list field through 'field ?: emptyList()'. The
USELESS_ELVIS warnings are suppressed because they are deliberately
load-bearing — the property type lies to the compiler about
non-null-ness, the elvis is the actual runtime safety net. Done for
ALL list fields, not just the ones we added in PR #333, so any future
addition stays robust against the same class of bug.

* docs: warn future maintainers about the normalized() obligation

Greptile P2 on PR #341: if a developer adds a new List field to
DatabaseSnapshot and forgets to add the matching '?: emptyList()'
line to BackupImporter.normalized(), the same NPE crash will
silently resurface for any backup that pre-dates that field — the
compiler gives no warning because the suppression already masks the
signal.

Add a prominent comment at the point of future additions explaining
why the Kotlin default initialiser isn't enough and what to update.
A more robust alternative would be a custom Gson InstanceCreator,
but it has the same enumeration burden

app/src/main/java/com/pennywiseai/tracker/data/backup/BackupImporter.kt
@@ -69,10 +69,48 @@ class BackupImporter @Inject constructor(
             context.contentResolver.openInputStream(uri)?.use { inputStream ->
                 val reader = BufferedReader(InputStreamReader(inputStream))
                 val content = reader.readText()
-                gson.fromJson(content, PennyWiseBackup::class.java)
+                val parsed = gson.fromJson(content, PennyWiseBackup::class.java)
+                parsed.copy(database = parsed.database.normalized())
             } ?: throw Exception("Failed to read backup file")
         }
     }
+
+    /**
+     * Gson sets every collection field via reflection, bypassing the Kotlin
+     * `= emptyList()` default initialisers on [DatabaseSnapshot]. Older
+     * backups that don't carry a given list key end up with `null` stored
+     * under a property whose declared type is non-null `List<T>` — any
+     * .forEach / .map / .isNotEmpty() call on it then NPEs at runtime even
+     * though the code looks safe.
+     *
+     * Run every collection field through an elvis to empty-list once, right
+     * after parsing, so the rest of the importer can keep its non-null
+     * assumptions. USELESS_ELVIS warnings are suppressed because they are
+     * deliberately load-bearing here.
+     */
+    @Suppress("USELESS_ELVIS")
+    private fun DatabaseSnapshot.normalized(): DatabaseSnapshot = copy(
+        transactions = transactions ?: emptyList(),
+        categories = categories ?: emptyList(),
+        cards = cards ?: emptyList(),
+        accountBalances = accountBalances ?: emptyList(),
+        subscriptions = subscriptions ?: emptyList(),
+        merchantMappings = merchantMappings ?: emptyList(),
+        unrecognizedSms = unrecognizedSms ?: emptyList(),
+        chatMessages = chatMessages ?: emptyList(),
+        rules = rules ?: emptyList(),
+        ruleApplications = ruleApplications ?: emptyList(),
+        exchangeRates = exchangeRates ?: emptyList(),
+        budgets = budgets ?: emptyList(),
+        budgetCategories = budgetCategories ?: emptyList(),
+        transactionSplits = transactionSplits ?: emptyList(),
+        bankNotifications = bankNotifications ?: emptyList(),
+        loans = loans ?: emptyList(),
+        transactionGroups = transactionGroups ?: emptyList(),
+        profiles = profiles ?: emptyList(),
+        budgetMonthSnapshots = budgetMonthSnapshots ?: emptyList(),
+        budgetCategoryMonthSnapshots = budgetCategoryMonthSnapshots ?: emptyList()
+    )
     
     /**
      * Check if backup version is compatible

ritesh-kanwar/Cashiro

app/src/main/java/com/ritesh/cashiro/data/backup/BackupImporter.kt
@@ -132,10 +132,48 @@ class BackupImporter @Inject constructor(
             context.contentResolver.openInputStream(uri)?.use { inputStream ->
                 val reader = BufferedReader(InputStreamReader(inputStream))
                 val content = reader.readText()
-                gson.fromJson(content, CashiroBackup::class.java)
+                val parsed = gson.fromJson(content, CashiroBackup::class.java)
+                parsed.copy(database = parsed.database.normalized())
             } ?: throw Exception("Failed to read backup file")
         }
     }
+
+    /**
+     * Gson sets every collection field via reflection, bypassing the Kotlin
+     * `= emptyList()` default initialisers on [DatabaseSnapshot]. Older
+     * backups that don't carry a given list key end up with `null` stored
+     * under a property whose declared type is non-null `List<T>` — any
+     * .forEach / .map / .isNotEmpty() call on it then NPEs at runtime even
+     * though the code looks safe.
+     *
+     * Run every collection field through an elvis to empty-list once, right
+     * after parsing, so the rest of the importer can keep its non-null
+     * assumptions. USELESS_ELVIS warnings are suppressed because they are
+     * deliberately load-bearing here.
+     */
+    @Suppress("USELESS_ELVIS")
+    private fun DatabaseSnapshot.normalized(): DatabaseSnapshot = copy(
+        transactions = transactions ?: emptyList(),
+        categories = categories ?: emptyList(),
+        cards = cards ?: emptyList(),
+        accountBalances = accountBalances ?: emptyList(),
+        subscriptions = subscriptions ?: emptyList(),
+        merchantMappings = merchantMappings ?: emptyList(),
+        unrecognizedSms = unrecognizedSms ?: emptyList(),
+        chatMessages = chatMessages ?: emptyList(),
+        rules = rules ?: emptyList(),
+        ruleApplications = ruleApplications ?: emptyList(),
+        exchangeRates = exchangeRates ?: emptyList(),
+        budgets = budgets ?: emptyList(),
+        budgetCategories = budgetCategories ?: emptyList(),
+        transactionSplits = transactionSplits ?: emptyList(),
+        bankNotifications = bankNotifications ?: emptyList(),
+        loans = loans ?: emptyList(),
+        transactionGroups = transactionGroups ?: emptyList(),
+        profiles = profiles ?: emptyList(),
+        budgetMonthSnapshots = budgetMonthSnapshots ?: emptyList(),
+        budgetCategoryMonthSnapshots = budgetCategoryMonthSnapshots ?: emptyList()
+    )
     
     /**
      * Check if backup version is compatible